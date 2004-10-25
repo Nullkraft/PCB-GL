0a1fe9ea646e52bb770fe5e3b3ddb4c01cbb89cc
danmc <danmc>
2004-10-25 10:55:24 +0000

    Add date and compile time to the About dialog.  Patch provided as patch #1053444 by Bob Paddock.

--- src/dialog.c
+++ src/dialog.c
@@ -1,4 +1,4 @@
-/* $Id: dialog.c,v 1.8 2004-09-24 02:55:26 djdelorie Exp $ */
+/* $Id: dialog.c,v 1.9 2004-10-25 10:55:24 danmc Exp $ */
 
 /*
  *                            COPYRIGHT
@@ -56,7 +56,7 @@
 #include <dmalloc.h>
 #endif
 
-RCSID("$Id: dialog.c,v 1.8 2004-09-24 02:55:26 djdelorie Exp $");
+RCSID("$Id: dialog.c,v 1.9 2004-10-25 10:55:24 danmc Exp $");
 
 
 
@@ -546,6 +546,7 @@ AboutDialog (void)
   popup = CreateDialogBox ("This is PCB, an interactive\n"
 			   "printed circuit board editor\n"
 			   "version " VERSION "\n\n"
+			   "Compiled on " __DATE__ " at " __TIME__ "\n\n"
 			   "by harry eaton\n\n"
 			   "Copyright (C) Thomas Nau 1994, 1995, 1996, 1997\n"
 			   "Copyright (C) harry eaton 1998-2004\n"
