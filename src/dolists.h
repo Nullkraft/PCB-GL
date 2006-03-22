1c21787d1cc41db948d369d4560d1cbcd6ea10ed
danmc <danmc>
2006-03-22 03:34:51 +0000

    add missing RCS Id

--- src/dolists.h
+++ src/dolists.h
@@ -1,3 +1,5 @@
+/* $Id: dolists.h,v 1.2 2006-03-22 03:34:51 danmc Exp $ */
+
 #undef REGISTER_ACTIONS
 #undef REGISTER_ATTRIBUTES
 #undef REGISTER_FLAGS
@@ -5,3 +7,4 @@
 #define REGISTER_ACTIONS(a) extern void HIDCONCAT(register_,a)();HIDCONCAT(register_,a)();
 #define REGISTER_ATTRIBUTES(a) extern void HIDCONCAT(register_,a)();HIDCONCAT(register_,a)();
 #define REGISTER_FLAGS(a) extern void HIDCONCAT(register_,a)();HIDCONCAT(register_,a)();
+
