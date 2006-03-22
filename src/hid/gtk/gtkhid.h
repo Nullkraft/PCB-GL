25e0a357133bf3c5d4008788090db7446cedaa61
danmc <danmc>
2006-03-22 23:02:10 +0000

    indent

--- src/hid/gtk/gtkhid.h
+++ src/hid/gtk/gtkhid.h
@@ -1,8 +1,7 @@
-/* $Id: gtkhid.h,v 1.1 2006-03-21 17:35:00 djdelorie Exp $ */
+/* $Id: gtkhid.h,v 1.2 2006-03-22 23:05:41 danmc Exp $ */
 
 #ifndef __GTKHID_H__
 #define __GTKHID_H__
 
 
 #endif /* __GTKHID_H__ */
-
