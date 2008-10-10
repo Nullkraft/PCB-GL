d48d4e75897a778eb6884e1f90fa4307222c5d9d
Peter Clifton <pcjc2@cam.ac.uk>
2008-10-10 11:30:31 +0100

    Refresh of make-pieces-of-a-clipped-polyg

--- src/global.h
+++ src/global.h
@@ -198,6 +198,7 @@ typedef struct
   void *Element;
 } TextType, *TextTypePtr;
 
+<<<<<<< current:src/global.h
 typedef struct			/* holds information about a polygon piece */
 {
   ANYOBJECTFIELDS;
@@ -208,6 +209,21 @@ typedef struct			/* holds information about a polygon piece */
 
 
 typedef struct			/* holds information about a polygon */
+=======
+typedef struct polygon_st PolygonType, *PolygonTypePtr;
+
+#if 0
+typedef struct			/* holds information about a poured area */
+{
+  ANYOBJECTFIELDS;
+  Cardinal PointN,		/* number of points in pour outline */
+    PointMax;			/* max number from malloc() */
+  PolygonTypePtr polygons;	/* the resulting polygons */
+} PourType, *PourTypePtr;
+#endif
+
+struct polygon_st		/* holds information about a polygon */
+>>>>>>> patched:src/global.h
 {
   ANYOBJECTFIELDS;
   PolygonPieceTypePtr Pieces;
@@ -218,7 +234,9 @@ typedef struct			/* holds information about a polygon */
   POLYAREA *NoHoles;		/* the polygon broken into hole-less regions */
   int NoHolesValid;		/* Is the NoHoles polygon up to date? */
   PointTypePtr Points;		/* data */
-} PolygonType, *PolygonTypePtr;
+//  PourTypePtr Pour;		/* The pour which resulted in this polygon */
+  PolygonTypePtr *Parent;	/* The parent polygon which defined us */
+};
 
 typedef struct			/* holds information about arcs */
 {
@@ -243,12 +261,14 @@ typedef struct			/* holds information about one layer */
   Cardinal LineN,		/* number of lines */
     TextN,			/* labels */
     PolygonN,			/* polygons */
+    PourN,			/* poured areas */
     ArcN,			/* and arcs */
     LineMax,			/* max number from malloc() */
-    TextMax, PolygonMax, ArcMax;
+    TextMax, PolygonMax, PourMax, ArcMax;
   LineTypePtr Line;		/* pointer to additional structures */
   TextTypePtr Text;
   PolygonTypePtr Polygon;
+  PolygonTypePtr Pour;
   ArcTypePtr Arc;
   rtree_t *line_tree, *text_tree, *polygon_tree, *arc_tree;
   Boolean On;			/* visible flag */
