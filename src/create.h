5e3ac75583e7c640159788fc145bb1ba3638d467
Peter Clifton <pcjc2@cam.ac.uk>
2008-10-22 00:30:33 +0100

    Add support for a "poured" object.
    This takes half of the existing polygon functionality, and splits it
    such that the user defines a poured region (similar to drawing a polygon
    previously), and after clipping, this produces zero or more polygon
    objects which are drawn and connectivity checked with the existing
    polygon code.
    
    Selecting a "polygon" will cause the parent "pour" to be looked up, and
    all child polygons selected. Manipulations will always occur on the
    "pour", with "polygons" being updated after clipping.
    
    It remains to be seen whether "pour" will have its own file-format entry,
    or if it will take over the meaning of the existing "polygon" entry.

--- src/create.h
+++ src/create.h
@@ -54,13 +54,14 @@ RatTypePtr CreateNewRat (DataTypePtr, LocationType, LocationType,
 ArcTypePtr CreateNewArcOnLayer (LayerTypePtr, LocationType, LocationType,
 				BDimension, BDimension, int, int, BDimension, BDimension,
 				FlagType);
-PolygonTypePtr CreateNewPolygonFromRectangle (LayerTypePtr, LocationType,
+PourTypePtr CreateNewPourFromRectangle (LayerTypePtr, LocationType,
 					      LocationType, LocationType,
 					      LocationType, FlagType);
 TextTypePtr CreateNewText (LayerTypePtr, FontTypePtr, LocationType,
 			   LocationType, BYTE, int, char *, FlagType);
-PolygonTypePtr CreateNewPolygon (LayerTypePtr, FlagType);
-PointTypePtr CreateNewPointInPolygon (PolygonTypePtr,
+PolygonTypePtr CreateNewPolygonInPour (PourType *pour, FlagType);
+PourTypePtr CreateNewPour (LayerTypePtr, FlagType);
+PointTypePtr CreateNewPointInPour (PourTypePtr,
 				      LocationType, LocationType);
 ElementTypePtr CreateNewElement (DataTypePtr, ElementTypePtr,
 				 FontTypePtr, FlagType, char *, char *,
