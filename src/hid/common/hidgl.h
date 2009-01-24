cd942b2ffa33af9af6c231299b2cd6db4de1b4b7
Peter Clifton <pcjc2@cam.ac.uk>
2009-01-24 02:07:21 +0000

    Add some shared code for GL drawing

---
+++ src/hid/common/hidgl.h
@@ -0,0 +1,37 @@
+/* $Id: gui.h,v 1.25 2008-04-13 14:15:38 petercjclifton Exp $ */
+
+/*
+ *                            COPYRIGHT
+ *
+ *  PCB, interactive printed circuit board design
+ *  Copyright (C) 1994,1995,1996 Thomas Nau
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef __HIDGL_INCLUDED__
+#define __HIDGL_INCLUDED__
+
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+////#include <GL/glut.h>
+
+int trans_lines;
+
+void hidgl_init_triangle_array ();
+void hidgl_flush_triangles ();
+
+#endif /* __HIDGL_INCLUDED__  */
