de02165948d2ef1e70a7dfa72c9d90f2b9b1a5d6
Peter Clifton <pcjc2@cam.ac.uk>
2009-02-02 05:06:17 +0000

    Refresh of improve-gl-stuff

--- src/hid/common/hidgl.c
+++ src/hid/common/hidgl.c
@@ -15,6 +15,12 @@
 #include <time.h>
 #include <assert.h>
 
+/* The Linux OpenGL ABI 1.0 spec requires that we define
+ * GL_GLEXT_PROTOTYPES before including gl.h or glx.h for extensions
+ * in order to get prototypes:
+ *   http://www.opengl.org/registry/ABI/
+ */
+#define GL_GLEXT_PROTOTYPES 1
 #include <GL/gl.h>
 #include <GL/glu.h>
 
@@ -41,8 +47,6 @@
 
 RCSID ("$Id: $");
 
-#define USE_GC(x)
-
 triangle_buffer buffer;
 
 #if 0
@@ -56,10 +60,33 @@ hidgl_new_triangle_array (void)
 void
 hidgl_init_triangle_array (triangle_buffer *buffer)
 {
-  glEnableClientState (GL_VERTEX_ARRAY);
-  glVertexPointer (2, GL_FLOAT, 0, buffer->triangle_array);
+  GLenum errCode;
+  const GLubyte *errString;
+
   buffer->triangle_count = 0;
   buffer->coord_comp_count = 0;
+
+  glEnableClientState (GL_VERTEX_ARRAY);
+  glGenBuffers (1, &buffer->vbo_name);
+  glBindBuffer (GL_ARRAY_BUFFER, buffer->vbo_name);
+                                                 /* HUGE BUFFER */
+//  glBufferData (GL_ARRAY_BUFFER, sizeof (GLFloat) * 8192 * 2 * 3, NULL, GL_STATIC_DRAW);
+  glBufferData (GL_ARRAY_BUFFER, TRIANGLE_ARRAY_BYTES, NULL, GL_STATIC_DRAW);
+//  glBufferSubData (GL_ARRAY_BUFFER, offsetInByte, SizeInBytes, &pvertices);
+//  glBindBuffer (GL_ARRAY_BUFFER, &buffer->vbo_name);
+//  glDeleteBuffers (1, &buffer->vbo_name);
+
+  buffer->triangle_array = glMapBuffer (GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+
+  if ((errCode = glGetError()) != GL_NO_ERROR) {
+      errString = gluErrorString(errCode);
+     fprintf (stderr, "OpenGL Error: %s\n", errString);
+  }
+
+  if (buffer->triangle_array == NULL) {
+    printf ("Couldn't map VBO.. sorry, don't know how best to handle this gracefully\n");
+    exit (1);
+  }
 }
 
 void
@@ -68,9 +95,18 @@ hidgl_flush_triangles (triangle_buffer *buffer)
   if (buffer->triangle_count == 0)
     return;
 
+  glUnmapBuffer (GL_ARRAY_BUFFER);
+
+  glVertexPointer (2, GL_FLOAT, 0, NULL); // buffer->triangle_array);
   glDrawArrays (GL_TRIANGLES, 0, buffer->triangle_count * 3);
-  buffer->triangle_count = 0;
-  buffer->coord_comp_count = 0;
+
+//  buffer->triangle_count = 0;
+//  buffer->coord_comp_count = 0;
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glDeleteBuffers (1, &buffer->vbo_name);
+
+  hidgl_init_triangle_array (buffer);
 }
 
 void
@@ -260,86 +296,42 @@ hidgl_use_mask (int use_it)
 #endif
 
 
-typedef struct
-{
-  int color_set;
-//  GdkColor color;
-  int xor_set;
-//  GdkColor xor_color;
-  double red;
-  double green;
-  double blue;
-} ColorCache;
-
-
-void
-hidgl_set_draw_xor (hidGC gc, int xor)
-{
-  // printf ("hidgl_set_draw_xor (%p, %d) -- not implemented\n", gc, xor);
-  /* NOT IMPLEMENTED */
-
-  /* Only presently called when setting up a crosshair GC.
-   * We manage our own drawing model for that anyway. */
-}
-
-void
-hidgl_set_draw_faded (hidGC gc, int faded)
-{
-  printf ("hidgl_set_draw_faded(%p,%d) -- not implemented\n", gc, faded);
-}
-
-void
-hidgl_set_line_cap_angle (hidGC gc, int x1, int y1, int x2, int y2)
-{
-  printf ("hidgl_set_line_cap_angle() -- not implemented\n");
-}
-
-#if 0
-static void
-use_gc (hidGC gc)
-{
-  static hidGC current_gc = NULL;
-
-  if (current_gc == gc)
-    return;
-
-  current_gc = gc;
-
-  hidgl_set_color (gc, gc->colorname);
-}
-#endif
-
-static void
-errorCallback(GLenum errorCode)
-{
-   const GLubyte *estring;
-
-   estring = gluErrorString(errorCode);
-   fprintf(stderr, "Quadric Error: %s\n", estring);
-//   exit(0);
-}
-
-
-void
-hidgl_draw_line (hidGC gc, int cap, double width, int x1, int y1, int x2, int y2)
-{
-#define TRIANGLES_PER_CAP 15
 #define MIN_TRIANGLES_PER_CAP 3
-#define MAX_TRIANGLES_PER_CAP 1000
+#define MAX_TRIANGLES_PER_CAP 90
+static void draw_cap (double width, int x, int y, double angle, double scale)
+{
+  float last_capx, last_capy;
+  float capx, capy;
+  float radius = width / 2.;
+  int slices = M_PI * radius / scale / PIXELS_PER_CIRCLINE;
+  int i;
+
+  if (slices < MIN_TRIANGLES_PER_CAP)
+    slices = MIN_TRIANGLES_PER_CAP;
+
+  if (slices > MAX_TRIANGLES_PER_CAP)
+    slices = MAX_TRIANGLES_PER_CAP;
+
+  hidgl_ensure_triangle_space (&buffer, slices);
+
+  last_capx =  radius * cosf (angle * M_PI / 180.) + x;
+  last_capy = -radius * sinf (angle * M_PI / 180.) + y;
+  for (i = 0; i < slices; i++) {
+    capx =  radius * cosf (angle * M_PI / 180. + ((float)(i + 1)) * M_PI / (float)slices) + x;
+    capy = -radius * sinf (angle * M_PI / 180. + ((float)(i + 1)) * M_PI / (float)slices) + y;
+    hidgl_add_triangle (&buffer, last_capx, last_capy, capx, capy, x, y);
+    last_capx = capx;
+    last_capy = capy;
+  }
+}
+
+void
+hidgl_draw_line (int cap, double width, int x1, int y1, int x2, int y2, double scale)
+{
   double angle;
   float deltax, deltay, length;
   float wdx, wdy;
-  int slices;
   int circular_caps = 0;
-
-#if 0
-  if (! ClipLine (0, 0, gport->width, gport->height,
-  if (! ClipLine (0, 0, gport->width, gport->height,
-                  &dx1, &dy1, &dx2, &dy2, gc->width / gport->zoom))
-    return;
-#endif
-
-  USE_GC (gc);
 
   if (width == 0.0)
     width = 1.0;
@@ -350,9 +342,13 @@ hidgl_draw_line (hidGC gc, int cap, double width, int x1, int y1, int x2, int y2
   length = sqrt (deltax * deltax + deltay * deltay);
 
   if (length == 0) {
+    /* Assume the orientation of the line is horizontal */
     angle = 0;
     wdx = -width / 2.;
     wdy = 0;
+    length = 1.;
+    deltax = 1.;
+    deltay = 0.;
   } else {
     wdy = deltax * width / 2. / length;
     wdx = -deltay * width / 2. / length;
@@ -365,16 +361,6 @@ hidgl_draw_line (hidGC gc, int cap, double width, int x1, int y1, int x2, int y2
     if (deltay < 0)
       angle += 180.;
   }
-
-  slices = M_PI * width / PIXELS_PER_CIRCLINE;
-
-  if (slices < MIN_TRIANGLES_PER_CAP)
-    slices = MIN_TRIANGLES_PER_CAP;
-
-  if (slices > MAX_TRIANGLES_PER_CAP)
-    slices = MAX_TRIANGLES_PER_CAP;
-
-//  slices = TRIANGLES_PER_CAP;
 
   switch (cap) {
     case Trace_Cap:
@@ -398,111 +384,82 @@ hidgl_draw_line (hidGC gc, int cap, double width, int x1, int y1, int x2, int y2
   hidgl_add_triangle (&buffer, x1 - wdx, y1 - wdy,
                                x2 + wdx, y2 + wdy,
                                x1 + wdx, y1 + wdy);
-
-  if (circular_caps) {
-    int i;
-    float last_capx, last_capy;
-
-    hidgl_ensure_triangle_space (&buffer, 2 * slices);
-
-    last_capx = ((float)width) / 2. * cos (angle * M_PI / 180.) + x1;
-    last_capy = -((float)width) / 2. * sin (angle * M_PI / 180.) + y1;
-    for (i = 0; i < slices; i++) {
-      float capx, capy;
-      capx = ((float)width) / 2. * cos (angle * M_PI / 180. + ((float)(i + 1)) * M_PI / (float)slices) + x1;
-      capy = -((float)width) / 2. * sin (angle * M_PI / 180. + ((float)(i + 1)) * M_PI / (float)slices) + y1;
-      hidgl_add_triangle (&buffer, last_capx, last_capy, capx, capy, x1, y1);
-      last_capx = capx;
-      last_capy = capy;
+  if (circular_caps)
+    {
+      draw_cap (width, x1, y1, angle, scale);
+      draw_cap (width, x2, y2, angle + 180., scale);
     }
-    last_capx = -((float)width) / 2. * cos (angle * M_PI / 180.) + x2;
-    last_capy = ((float)width) / 2. * sin (angle * M_PI / 180.) + y2;
-    for (i = 0; i < slices; i++) {
-      float capx, capy;
-      capx = -((float)width) / 2. * cos (angle * M_PI / 180. + ((float)(i + 1)) * M_PI / (float)slices) + x2;
-      capy = ((float)width) / 2. * sin (angle * M_PI / 180. + ((float)(i + 1)) * M_PI / (float)slices) + y2;
-      hidgl_add_triangle (&buffer, last_capx, last_capy, capx, capy, x2, y2);
-      last_capx = capx;
-      last_capy = capy;
-    }
-  }
 }
 
+#define MIN_SLICES_PER_ARC 6
+#define MAX_SLICES_PER_ARC 360
 void
-hidgl_draw_arc (hidGC gc, double width, int vx, int vy,
-               int vrx, int vry, int start_angle, int delta_angle,
-               int flip_x, int flip_y)
+hidgl_draw_arc (double width, int x, int y, int rx, int ry,
+                int start_angle, int delta_angle, double scale)
 {
-#define MIN_SLICES_PER_ARC 10
+  float last_inner_x, last_inner_y;
+  float last_outer_x, last_outer_y;
+  float inner_x, inner_y;
+  float outer_x, outer_y;
+  float inner_r;
+  float outer_r;
+  float cos_ang, sin_ang;
+  float start_angle_rad;
+  float delta_angle_rad;
+  float angle_incr_rad;
   int slices;
-  GLUquadricObj *qobj;
-
-  USE_GC (gc);
+  int i;
 
   if (width == 0.0)
     width = 1.0;
 
-  if (flip_x)
-    {
-      start_angle = 180 - start_angle;
-      delta_angle = - delta_angle;
-    }
-  if (flip_y)
-    {
-      start_angle = - start_angle;
-      delta_angle = - delta_angle;
-    }
-  /* make sure we fall in the -180 to +180 range */
-  start_angle = (start_angle + 360 + 180) % 360 - 180;
-  start_angle = (start_angle + 360 + 180) % 360 - 180;
+  inner_r = rx - width / 2.;
+  outer_r = rx + width / 2.;
 
-  if (delta_angle < 0) {
-    start_angle += delta_angle;
-    delta_angle = - delta_angle;
-  }
-
-  slices = M_PI * (vrx + width / 2.) / PIXELS_PER_CIRCLINE;
+  slices = M_PI * (rx + width / 2.) / scale / PIXELS_PER_CIRCLINE;
 
   if (slices < MIN_SLICES_PER_ARC)
     slices = MIN_SLICES_PER_ARC;
 
-  /* TODO: CHANGE TO USING THE TRIANGLE LIST */
-  qobj = gluNewQuadric ();
-  gluQuadricCallback (qobj, GLU_ERROR, errorCallback);
-  gluQuadricDrawStyle (qobj, GLU_FILL); /* smooth shaded */
-  gluQuadricNormals (qobj, GLU_SMOOTH);
+  if (slices > MAX_SLICES_PER_ARC)
+    slices = MAX_SLICES_PER_ARC;
 
-  glPushMatrix ();
-  glTranslatef (vx, vy, 0.0);
-  gluPartialDisk (qobj, vrx - width / 2, vrx + width / 2, slices, 1, 270 + start_angle, delta_angle);
-  glPopMatrix ();
+  hidgl_ensure_triangle_space (&buffer, 2 * slices);
 
-  slices = M_PI * width / PIXELS_PER_CIRCLINE;
+  start_angle_rad = start_angle * M_PI / 180.;
+  delta_angle_rad = delta_angle * M_PI / 180.;
+  angle_incr_rad = delta_angle_rad / (float)slices;
 
-  if (slices < MIN_TRIANGLES_PER_CAP)
-    slices = MIN_TRIANGLES_PER_CAP;
+  cos_ang = cosf (start_angle_rad);
+  sin_ang = sinf (start_angle_rad);
+  last_inner_x = -inner_r * cos_ang + x;  last_inner_y = inner_r * sin_ang + y;
+  last_outer_x = -outer_r * cos_ang + x;  last_outer_y = outer_r * sin_ang + y;
+  for (i = 1; i <= slices; i++) {
+    cos_ang = cosf (start_angle_rad + ((float)(i)) * angle_incr_rad);
+    sin_ang = sinf (start_angle_rad + ((float)(i)) * angle_incr_rad);
+    inner_x = -inner_r * cos_ang + x;  inner_y = inner_r * sin_ang + y;
+    outer_x = -outer_r * cos_ang + x;  outer_y = outer_r * sin_ang + y;
+    hidgl_add_triangle (&buffer, last_inner_x, last_inner_y,
+                                 last_outer_x, last_outer_y,
+                                 outer_x, outer_y);
+    hidgl_add_triangle (&buffer, last_inner_x, last_inner_y,
+                                 inner_x, inner_y,
+                                 outer_x, outer_y);
+    last_inner_x = inner_x;  last_inner_y = inner_y;
+    last_outer_x = outer_x;  last_outer_y = outer_y;
+  }
 
-  /* TODO: CHANGE TO USING THE TRIANGLE LIST */
-  glPushMatrix ();
-  glTranslatef (vx + vrx * -cos (M_PI / 180. * start_angle),
-                vy + vrx *  sin (M_PI / 180. * start_angle), 0.0);
-  gluPartialDisk (qobj, 0, width / 2, slices, 1, start_angle + 90., 180);
-  glPopMatrix ();
-
-  /* TODO: CHANGE TO USING THE TRIANGLE LIST */
-  glPushMatrix ();
-  glTranslatef (vx + vrx * -cos (M_PI / 180. * (start_angle + delta_angle)),
-                vy + vrx *  sin (M_PI / 180. * (start_angle + delta_angle)), 0.0);
-  gluPartialDisk (qobj, 0, width / 2, slices, 1, start_angle + delta_angle + 270., 180);
-  glPopMatrix ();
-
-  gluDeleteQuadric (qobj);
+  draw_cap (width, x + rx * -cosf (start_angle_rad),
+                   y + rx *  sinf (start_angle_rad),
+                   start_angle, scale);
+  draw_cap (width, x + rx * -cosf (start_angle_rad + delta_angle_rad),
+                   y + rx *  sinf (start_angle_rad + delta_angle_rad),
+                   start_angle + delta_angle + 180., scale);
 }
 
 void
-hidgl_draw_rect (hidGC gc, int x1, int y1, int x2, int y2)
+hidgl_draw_rect (int x1, int y1, int x2, int y2)
 {
-  USE_GC (gc);
   glBegin (GL_LINE_LOOP);
   glVertex2f (x1, y1);
   glVertex2f (x1, y2);
@@ -513,26 +470,22 @@ hidgl_draw_rect (hidGC gc, int x1, int y1, int x2, int y2)
 
 
 void
-hidgl_fill_circle (hidGC gc, int vx, int vy, int vr)
+hidgl_fill_circle (int vx, int vy, int vr, double scale)
 {
-#define TRIANGLES_PER_CIRCLE 30
 #define MIN_TRIANGLES_PER_CIRCLE 6
-#define MAX_TRIANGLES_PER_CIRCLE 2000
+#define MAX_TRIANGLES_PER_CIRCLE 360
   float last_x, last_y;
+  float radius = vr;
   int slices;
   int i;
 
-  USE_GC (gc);
-
-  slices = M_PI * 2 * vr / PIXELS_PER_CIRCLINE;
+  slices = M_PI * 2 * vr / scale / PIXELS_PER_CIRCLINE;
 
   if (slices < MIN_TRIANGLES_PER_CIRCLE)
     slices = MIN_TRIANGLES_PER_CIRCLE;
 
   if (slices > MAX_TRIANGLES_PER_CIRCLE)
     slices = MAX_TRIANGLES_PER_CIRCLE;
-
-//  slices = TRIANGLES_PER_CIRCLE;
 
   hidgl_ensure_triangle_space (&buffer, slices);
 
@@ -541,8 +494,8 @@ hidgl_fill_circle (hidGC gc, int vx, int vy, int vr)
 
   for (i = 0; i < slices; i++) {
     float x, y;
-    x = ((float)vr) * cos (((float)(i + 1)) * 2. * M_PI / (float)slices) + vx;
-    y = ((float)vr) * sin (((float)(i + 1)) * 2. * M_PI / (float)slices) + vy;
+    x = radius * cosf (((float)(i + 1)) * 2. * M_PI / (float)slices) + vx;
+    y = radius * sinf (((float)(i + 1)) * 2. * M_PI / (float)slices) + vy;
     hidgl_add_triangle (&buffer, vx, vy, last_x, last_y, x, y);
     last_x = x;
     last_y = y;
@@ -664,14 +617,13 @@ myVertex (GLdouble *vertex_data)
 }
 
 void
-hidgl_fill_polygon (hidGC gc, int n_coords, int *x, int *y)
+hidgl_fill_polygon (int n_coords, int *x, int *y)
 {
   int i;
-
   GLUtesselator *tobj;
   GLdouble *vertices;
 
-  USE_GC (gc);
+//  return;
 
   assert (n_coords > 0);
 
@@ -703,9 +655,8 @@ hidgl_fill_polygon (hidGC gc, int n_coords, int *x, int *y)
 }
 
 void
-hidgl_fill_rect (hidGC gc, int x1, int y1, int x2, int y2)
+hidgl_fill_rect (int x1, int y1, int x2, int y2)
 {
-  USE_GC (gc);
   glBegin (GL_QUADS);
   glVertex2f (x1, y1);
   glVertex2f (x1, y2);
