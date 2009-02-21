d2f2cc6b0dfb901dcabec90b34b78cc66d05c08d
Peter Clifton <pcjc2@cam.ac.uk>
2009-02-21 02:37:53 +0000

    Debug polygon stuff

--- src/hid/gtk/gtkhid-main.c
+++ src/hid/gtk/gtkhid-main.c
@@ -1488,7 +1488,7 @@ HID ghid_hid = {
   ghid_draw_rect,
   ghid_fill_circle,
   ghid_fill_polygon,
-  ghid_fill_pcb_polygon,
+  NULL, // ghid_fill_pcb_polygon,
   ghid_fill_rect,
 
   ghid_calibrate,
