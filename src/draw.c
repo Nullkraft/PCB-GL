8e0608b9d6b561fb792fd93d4a33ff0aa38ce904
Peter Clifton <pcjc2@cam.ac.uk>
2009-02-23 22:45:37 +0000

    Off to find burried treasure on polygon island
    Add feature to remove islanded areas

--- src/draw.c
+++ src/draw.c
@@ -2257,6 +2257,10 @@ DrawPlainPolygon (LayerTypePtr Layer, PolygonTypePtr Polygon)
   if (!Polygon->Clipped)
     return;
 
+  /* Re-use HOLEFLAG to cut out islands */
+  if (TEST_FLAG (HOLEFLAG, Polygon))
+    return;
+
   if (Gathering)
     {
       AddPart (Polygon);
