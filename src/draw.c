1c5a8233ff02e3ff007e16a66a3d71e6bf39a6ee
Peter Clifton <pcjc2@cam.ac.uk>
2009-10-21 00:41:32 +0100

    Refresh of work-in-progress-on-subcomposi

--- src/draw.c
+++ src/draw.c
@@ -485,7 +485,7 @@ DrawEverything (BoxTypePtr drawn_area)
       SWAP_IDENT = 0;
       DrawMask (drawn_area);
       SWAP_IDENT = save_swap;
-      gui->set_layer (NULL,  SL (FINISHED, 0), 0);
+      gui->set_layer (NULL, SL (FINISHED, 0), 0);
     }
   if (gui->set_layer ("soldermask", SL (MASK, BOTTOM), 0))
     {
