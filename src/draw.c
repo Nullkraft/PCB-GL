05cd19a9628330ed2032005940de651b1d2cedfe
Peter Clifton <pcjc2@cam.ac.uk>
2009-10-21 21:59:02 +0100

    Refresh of major-re-write-to-drawing-rout

--- src/draw.c
+++ src/draw.c
@@ -414,8 +414,6 @@ DrawEverything (BoxTypePtr drawn_area)
   /* This is the reverse of the order in which we draw them.  */
   int drawn_groups[MAX_LAYER];
 
-  printf ("WARNING\n");
-
   PCB->Data->SILKLAYER.Color = PCB->ElementColor;
   PCB->Data->BACKSILKLAYER.Color = PCB->InvisibleObjectsColor;
 
