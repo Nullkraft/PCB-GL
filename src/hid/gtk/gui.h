e3dae6d27d30353920cfbab2dbd77d1469460758
Peter Clifton <pcjc2@cam.ac.uk>
2009-10-22 14:47:23 +0100

    Major re-write to drawing routines

--- src/hid/gtk/gui.h
+++ src/hid/gtk/gui.h
@@ -561,6 +561,7 @@ GdkPixmap *ghid_render_pixmap (int cx,
 /* gtkhid-main.c */
 
 void ghid_invalidate_all ();
+void ghid_global_alpha_mult (hidGC gc, double alpha_mult);
 void ghid_invalidate_current_gc ();
 void ghid_get_coords (const char *msg, int *x, int *y);
 gint PCBChanged (int argc, char **argv, int x, int y);
