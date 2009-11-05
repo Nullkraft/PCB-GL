612ee51e65578fcdec4e08f39f87a197233c7231
Peter Clifton <pcjc2@cam.ac.uk>
2009-11-05 17:03:08 +0000

    Work in progress

--- src/polygon1.c
+++ src/polygon1.c
@@ -720,7 +720,7 @@ make_edge_tree (PLINE * pb)
 	}
       s->v = bv;
       s->p = pb;
-      r_insert_entry (ans, (const BoxType *) s, 1);
+//      r_insert_entry (ans, (const BoxType *) s, 1);
     }
   while ((bv = bv->next) != &pb->head);
   return (void *) ans;
@@ -985,7 +985,7 @@ M_POLYAREA_intersect (jmp_buf * e, POLYAREA * afst, POLYAREA * bfst, int add)
       do {
         num_new_nodes = bo_intersect (e, a, b, first_pass);
         if (!first_pass && num_new_nodes != 0)
-          printf ("Got %i new nodes with another pass\n", num_new_nodes);
+//          printf ("Got %i new nodes with another pass\n", num_new_nodes);
         first_pass = 0;
       } while (num_new_nodes != 0);
 
