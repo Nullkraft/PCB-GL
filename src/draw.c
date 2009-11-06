d4f295687745ed2bc7dc77c5d5ca34847feb47cf
Peter Clifton <pcjc2@cam.ac.uk>
2009-11-06 16:06:24 +0000

    Allow polygons with CLEARLINEFLAG set to cut out other polygons
    Gives the possibility to mask out regions which are to be free of
    polygon fill.

--- src/draw.c
+++ src/draw.c
@@ -2115,7 +2115,8 @@ DrawPlainPolygon (LayerTypePtr Layer, PolygonTypePtr Polygon)
 
   if (gui->thindraw_pcb_polygon != NULL &&
       (TEST_FLAG (THINDRAWFLAG, PCB) ||
-       TEST_FLAG (THINDRAWPOLYFLAG, PCB)))
+       TEST_FLAG (THINDRAWPOLYFLAG, PCB) ||
+       TEST_FLAG (CLEARLINEFLAG, Polygon)))
     gui->thindraw_pcb_polygon (Output.fgGC, Polygon, clip_box);
   else
     gui->fill_pcb_polygon (Output.fgGC, Polygon, clip_box);
