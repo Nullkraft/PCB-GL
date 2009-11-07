291bb321bd877769f5cfe1ccc46585f74ca833c4
Peter Clifton <pcjc2@cam.ac.uk>
2009-11-07 10:00:28 +0000

    Add magic flag to stop pour clipping for off-screen buffers

--- src/pour.c
+++ src/pour.c
@@ -1283,6 +1283,8 @@ PlowPours (DataType * Data, int type, void *ptr1, void *ptr2,
 void
 RestoreToPours (DataType * Data, int type, void *ptr1, void *ptr2)
 {
+  if (!Data->ClipPours)
+    return;
   if (type == POUR_TYPE)
     {
 #warning FIXME Later: Why do we need to do this?
@@ -1295,6 +1297,8 @@ RestoreToPours (DataType * Data, int type, void *ptr1, void *ptr2)
 void
 ClearFromPours (DataType * Data, int type, void *ptr1, void *ptr2)
 {
+  if (!Data->ClipPours)
+    return;
   if (type == POUR_TYPE)
     {
 #warning FIXME Later: Why do we need to do this?
