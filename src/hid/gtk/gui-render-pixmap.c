bf74ebd0300558335ac19f947620cd0f5d4db4aa
Peter Clifton <pcjc2@cam.ac.uk>
2009-11-14 18:41:55 +0000

    Drop in PCB+GL code + various mess. Needs splitting up?
    Credits:
    
      Algorithm to calculate number of segments to use in circular
      curve approximation suggested by DJ Delorie.

--- src/hid/gtk/gui-render-pixmap.c
+++ src/hid/gtk/gui-render-pixmap.c
@@ -39,19 +39,24 @@
 RCSID ("$Id$");
 
 
+#define Z_NEAR 3.0
+
+
 GdkPixmap *
 ghid_render_pixmap (int cx, int cy, double zoom, int width, int height, int depth)
 {
   extern HID ghid_hid;
+  GdkGLConfig *glconfig;
   GdkPixmap *pixmap;
-  GdkDrawable *save_drawable;
+  GdkGLPixmap *glpixmap;
+  GdkGLContext* glcontext;
+  GdkGLDrawable* gldrawable;
   double save_zoom;
   int save_left, save_top;
   int save_width, save_height;
   int save_view_width, save_view_height;
   BoxType region;
 
-  save_drawable = gport->drawable;
   save_zoom = gport->zoom;
   save_width = gport->width;
   save_height = gport->height;
@@ -60,12 +65,21 @@ ghid_render_pixmap (int cx, int cy, double zoom, int width, int height, int dept
   save_view_width = gport->view_width;
   save_view_height = gport->view_height;
 
-  pixmap = gdk_pixmap_new (NULL, width, height, depth);
-
-  /* Setup drawable and zoom factor for drawing routines
+  /* Setup rendering context for drawing routines
    */
 
-  gport->drawable = pixmap;
+  glconfig = gdk_gl_config_new_by_mode (GDK_GL_MODE_RGB     |
+                                        GDK_GL_MODE_STENCIL |
+//                                        GDK_GL_MODE_DEPTH   |
+                                        GDK_GL_MODE_SINGLE);
+
+  pixmap = gdk_pixmap_new (NULL, width, height, depth);
+  glpixmap = gdk_pixmap_set_gl_capability (pixmap, glconfig, NULL);
+  gldrawable = GDK_GL_DRAWABLE (glpixmap);
+  glcontext = gdk_gl_context_new (gldrawable, NULL, FALSE, GDK_GL_RGBA_TYPE);
+
+  /* Setup zoom factor for drawing routines */
+
   gport->zoom = zoom;
   gport->width = width;
   gport->height = height;
@@ -76,17 +90,62 @@ ghid_render_pixmap (int cx, int cy, double zoom, int width, int height, int dept
   gport->view_y0 = ghid_flip_y ? PCB->MaxHeight - cy : cy;
   gport->view_y0 -= gport->view_width  / 2;
 
-  /* clear background */
-  gdk_draw_rectangle (pixmap, gport->bg_gc, TRUE, 0, 0, width, height);
+  /* make GL-context "current" */
+  if (!gdk_gl_drawable_gl_begin (gldrawable, glcontext)) {
+    return NULL;
+  }
+
+  glEnable (GL_BLEND);
+  glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glViewport (0, 0, width, height);
+
+  glEnable (GL_SCISSOR_TEST);
+  glScissor (0, 0, width, height);
+
+  glMatrixMode (GL_PROJECTION);
+  glLoadIdentity ();
+  glOrtho (0, width, height, 0, 0, 100);
+  glMatrixMode (GL_MODELVIEW);
+  glLoadIdentity ();
+  glTranslatef (0.0f, 0.0f, -Z_NEAR);
+
+  glClearColor (gport->bg_color.red / 65535.,
+                gport->bg_color.green / 65535.,
+                gport->bg_color.blue / 65535.,
+                1.);
+  glClearStencil (0);
+  glClear (GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
   /* call the drawing routine */
+  hidgl_init_triangle_array (&buffer);
+  ghid_invalidate_current_gc ();
+  glPushMatrix ();
+  glScalef ((ghid_flip_x ? -1. : 1.) / gport->zoom,
+            (ghid_flip_y ? -1. : 1.) / gport->zoom, 1);
+  glTranslatef (ghid_flip_x ? gport->view_x0 - PCB->MaxWidth  :
+                             -gport->view_x0,
+                ghid_flip_y ? gport->view_y0 - PCB->MaxHeight :
+                             -gport->view_y0, 0);
   region.X1 = MIN(Px(0), Px(gport->width + 1));
   region.Y1 = MIN(Py(0), Py(gport->height + 1));
   region.X2 = MAX(Px(0), Px(gport->width + 1));
   region.Y2 = MAX(Py(0), Py(gport->height + 1));
   hid_expose_callback (&ghid_hid, &region, NULL);
+  hidgl_flush_triangles (&buffer);
+  glPopMatrix ();
 
-  gport->drawable = save_drawable;
+  glFlush ();
+
+  /* end drawing to current GL-context */
+  gdk_gl_drawable_gl_end (gldrawable);
+//  gdk_gl_context_destroy (glcontext);
+
+  gdk_pixmap_unset_gl_capability (pixmap);
+
+  g_object_unref (glconfig);
+  g_object_unref (glcontext);
+
   gport->zoom = save_zoom;
   gport->width = save_width;
   gport->height = save_height;
