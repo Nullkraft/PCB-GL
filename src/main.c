414171e3922e000bc31e42d9ee93b45f372f8ca7
Peter Clifton <pcjc2@cam.ac.uk>
2010-03-02 18:42:56 +0000

    Bentley-Ottann test implementation

--- src/main.c
+++ src/main.c
@@ -53,6 +53,7 @@
 #include "action.h"
 #include "misc.h"
 #include "lrealpath.h"
+#include "sweep.h"
 
 /* This next one is so we can print the help messages. */
 #include "hid/hidint.h"
@@ -860,6 +861,7 @@ char *program_directory = 0;
 
 #include "dolists.h"
 
+#if 1
 int
 main (int argc, char *argv[])
 {
@@ -1066,7 +1068,10 @@ main (int argc, char *argv[])
 	      UNKNOWN (Settings.GnetlistProgram));
 #endif
 
-      gui->do_export (0);
+//      bos_test_intersections ();
+      my_cairo_test ();
+
+      //gui->do_export (0);
 #if HAVE_DBUS
       pcb_dbus_finish();
 #endif
@@ -1074,4 +1079,5 @@ main (int argc, char *argv[])
 
   return (0);
 }
+#endif
 
