4a9d8ca43e4383ab3e7e5da7fa4be26607bab119
Peter Clifton <pcjc2@cam.ac.uk>
2010-03-11 23:47:43 +0000

    Allow running of action scripts when running PCB as an exporter

--- src/main.c
+++ src/main.c
@@ -996,12 +996,6 @@ main (int argc, char *argv[])
       LayerStringToLayerStack (Settings.InitialLayerStack);
     }
 
-  if (gui->printer || gui->exporter)
-    {
-      gui->do_export (0);
-      exit (0);
-    }
-
   /*    FIX_ME
      LoadBackgroundImage (Settings.BackgroundImage); */
 
@@ -1032,6 +1026,12 @@ main (int argc, char *argv[])
     {
       Message (_("Executing startup action %s\n"), Settings.ActionString);
       hid_parse_actions (Settings.ActionString, 0);
+    }
+
+  if (gui->printer || gui->exporter)
+    {
+      gui->do_export (0);
+      exit (0);
     }
 
 #if HAVE_DBUS
