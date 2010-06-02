c13e0875b6cbf1b1af453c642f591b3d952da6f6
Peter Clifton <pcjc2@cam.ac.uk>
2010-06-02 19:56:49 +0100

    Revert "Don't accumulate lines together"
    This reverts commit f58ef454c8e76a4efec12d99bcf4e7d967d5f265.

--- src/polygon.c
+++ src/polygon.c
@@ -110,7 +110,7 @@ RCSID ("$Id$");
 
 #define UNSUBTRACT_BLOAT 10
 #define SUBTRACT_PIN_VIA_BATCH_SIZE 100
-#define SUBTRACT_LINE_BATCH_SIZE 1
+#define SUBTRACT_LINE_BATCH_SIZE 20
 
 /* ---------------------------------------------------------------------------
  * local prototypes
