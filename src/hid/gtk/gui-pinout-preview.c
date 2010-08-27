c07472c93ad52c763c7d9661b914113e1ad3311c
Peter Clifton <pcjc2@cam.ac.uk>
2010-08-27 15:27:57 +0100

    Try to make GDK and GL versions coexist a little more!

--- src/hid/gtk/gui-pinout-preview.c
+++ src/hid/gtk/gui-pinout-preview.c
@@ -182,11 +182,7 @@ ghid_pinout_preview_constructor (GType type,
   object = G_OBJECT_CLASS (ghid_pinout_preview_parent_class)->
     constructor (type, n_construct_properties, construct_properties);
 
-  gtk_widget_set_gl_capability (GTK_WIDGET (object),
-                                gport->glconfig,
-                                NULL,
-                                TRUE,
-                                GDK_GL_RGBA_TYPE);
+  ghid_pinout_preview_init (GHID_PINOUT_PREVIEW (object));
 
   return object;
 }
