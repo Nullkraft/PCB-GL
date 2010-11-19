92a88953552cf6751d085ba93dee9df927a08db8
Peter Clifton <pcjc2@cam.ac.uk>
2010-11-19 05:07:13 +0000

    Add VRML model rendering support

--- src/hid/common/hidgl.h
+++ src/hid/common/hidgl.h
@@ -141,6 +141,10 @@ void hidgl_reset_stencil_usage (void);
 void hidgl_set_depth (float depth);
 
 /* hidgl_pacakge_acy_resistor.c */
+void hidgl_init_acy_resistor ();
 void hidgl_draw_acy_resistor (ElementType *element, float surface_depth, float board_thickness);
+
+/* hidgl_pacakge_vrml.c */
+void hidgl_draw_vrml (ElementType *element, float surface_depth, float board_thickness, char *vrml_file);
 
 #endif /* __HIDGL_INCLUDED__  */
