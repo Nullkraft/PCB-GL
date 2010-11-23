5237ab73603111e99f3b2976cff5083947da4e7e
Peter Clifton <pcjc2@cam.ac.uk>
2010-11-23 15:32:47 +0000

    Disable DRC checks on "outline" and "route" layers

--- src/misc.h
+++ src/misc.h
@@ -138,6 +138,8 @@ char * GetInfoString (void);
    comparing two similar footprints.  */
 int ElementOrientation (ElementType *e);
 
+bool is_layer_mechanical (LayerType *layer);
+
 /* These are in netlist.c */
 
 void NetlistChanged (int force_unfreeze);
