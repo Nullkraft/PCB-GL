d73b0d512ba12baa692e34b669f986450dd41f74
Peter Clifton <pcjc2@cam.ac.uk>
2010-12-09 19:27:40 +0000

    Refresh of hid-make-arguments-to-the-prom

--- src/hid/lesstif/dialogs.c
+++ src/hid/lesstif/dialogs.c
@@ -544,7 +544,7 @@ lesstif_fileselect (const char *title, const char *descr,
 		    const char *history_tag, int flags)
 {
 
-  return lesstif_prompt_for ((char *)title, default_file);
+  return lesstif_prompt_for (title, default_file);
 }
 
 /* ------------------------------------------------------------ */
@@ -553,7 +553,7 @@ static Widget prompt_dialog = 0;
 static Widget prompt_label, prompt_text;
 
 char *
-lesstif_prompt_for (char *msg, char *default_string)
+lesstif_prompt_for (const char *msg, const char *default_string)
 {
   char *rv;
   XmString xs;
