582419d280cf8b99d26a0931c6e1c1d59765b616
Peter Clifton <pcjc2@cam.ac.uk>
2010-12-23 16:20:11 +0000

    Refresh of expose-resetconnections-api-an

--- src/set.c
+++ src/set.c
@@ -292,10 +292,7 @@ SetMode (int Mode)
       Crosshair.AttachedLine.State = STATE_FIRST;
       if (Mode == LINE_MODE && TEST_FLAG (AUTODRCFLAG, PCB))
 	{
-	  SaveUndoSerialNumber ();
-	  ResetFoundPinsViasAndPads (true);
-	  RestoreUndoSerialNumber ();
-	  ResetFoundLinesAndPolygons (true);
+	  ResetFoundConnections (true);
 	  IncrementUndoSerialNumber ();
 	}
     }
