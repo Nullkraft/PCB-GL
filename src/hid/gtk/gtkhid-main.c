1fe386a27dd5db0bd8825275ad2f01ce9045b32d
Peter Clifton <pcjc2@cam.ac.uk>
2011-03-26 16:13:02 +0000

    Drop in PCB+GL code + various mess. Needs splitting up?
    Credits:
    
      Algorithm to calculate number of segments to use in circular
      curve approximation suggested by DJ Delorie.
    
      Thanks to Krzysztof Kościuszkiewicz for debugging an issue where
      I falied to disable the GL_SCISSOR_TEST before clearing buffers.
        edit: Not sure about this - did it turn out to be something else?

--- src/hid/gtk/gtkhid-main.c
+++ src/hid/gtk/gtkhid-main.c
@@ -979,7 +979,7 @@ HID ghid_hid = {
   1,				/* gui */
   0,				/* printer */
   0,				/* exporter */
-  0,				/* poly before */
+  1,				/* poly before */
   1,				/* poly after */
   0,				/* poly dicer */
 
@@ -1562,7 +1562,8 @@ Benchmark (int argc, char **argv, int x, int y)
   time (&start);
   do
     {
-      hid_expose_callback (&ghid_hid, &region, 0);
+      gdk_window_invalidate_rect (gport->drawing_area->window, NULL, 1);
+      gdk_window_process_updates (gport->drawing_area->window, FALSE);
       gdk_display_sync (display);
       time (&end);
       i++;
