1076bb2f16e29e0db182707b443cddf11e5faa64
Peter Clifton <pcjc2@cam.ac.uk>
2011-03-27 15:18:21 +0100

    Refresh of cache-polygon-contour-tri-stri

--- src/sweep.h
+++ src/sweep.h
@@ -2,3 +2,5 @@
 
 cairo_status_t bo_poly_to_traps (POLYAREA *poly, cairo_traps_t *traps);
 cairo_status_t bo_contour_to_traps (PLINE *contour, cairo_traps_t *traps);
+cairo_status_t bo_contour_to_traps_no_draw (PLINE *contour, cairo_traps_t *traps);
+cairo_fixed_t _line_compute_intersection_x_for_y (const cairo_line_t *line, cairo_fixed_t y);
