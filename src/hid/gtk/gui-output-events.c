3a88a475b36bb641ac8ba63ffdd29d7944eb64e7
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-06 22:28:14 +0100

    Refresh of hid-gtk-consolidate-the-mechan

--- src/hid/gtk/gui-output-events.c
+++ src/hid/gtk/gui-output-events.c
@@ -58,12 +58,6 @@ ghid_port_ranges_changed (void)
 {
   GtkAdjustment *h_adj, *v_adj;
 
-  if (ghidgui->combine_adjustments)
-    {
-      ghidgui->combine_adjustments = FALSE;
-      return;
-    }
-
   h_adj = gtk_range_get_adjustment (GTK_RANGE (ghidgui->h_range));
   v_adj = gtk_range_get_adjustment (GTK_RANGE (ghidgui->v_range));
   gport->view_x0 = h_adj->value;
@@ -104,12 +98,15 @@ ghid_port_ranges_pan (gdouble x, gdouble y, gboolean relative)
   if (y1 > v_adj->upper - v_adj->page_size)
     y1 = v_adj->upper - v_adj->page_size;
 
-  if (x0 != x1 && y0 != y1)
-    ghidgui->combine_adjustments = TRUE;
-  if (x0 != x1)
-    gtk_range_set_value (GTK_RANGE (ghidgui->h_range), x1);
-  if (y0 != y1)
-    gtk_range_set_value (GTK_RANGE (ghidgui->v_range), y1);
+  ghid_screen_update ();
+
+  ghidgui->adjustment_changed_holdoff = TRUE;
+  gtk_range_set_value (GTK_RANGE (ghidgui->h_range), x1);
+  gtk_range_set_value (GTK_RANGE (ghidgui->v_range), y1);
+  ghidgui->adjustment_changed_holdoff = FALSE;
+
+  if ((x0 != x1) || (y0 != y1))
+    ghid_port_ranges_changed();
 
   ghid_note_event_location (NULL);
   return ((x0 != x1) || (y0 != y1));
