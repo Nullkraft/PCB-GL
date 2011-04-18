ed96d9f8b0ddcc9229ae7f637d37e2eee9bb1441
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-18 23:05:57 +0100

    Refresh of give-the-hids-control-over-att

--- src/draw.c
+++ src/draw.c
@@ -186,13 +186,9 @@ UpdateAll (void)
 void
 Draw (void)
 {
-  HideCrosshair ();
-
   /* clear and create event if not drawing to a pixmap
    */
   gui->invalidate_lr (Block.X1, Block.X2, Block.Y1, Block.Y2);
-
-  RestoreCrosshair ();
 
   /* shrink the update block */
   Block.X1 = Block.Y1 = Block.X2 = Block.Y2 = 0;
