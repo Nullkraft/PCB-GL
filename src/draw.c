18041faadb32082fa65da97d4ddfc24e7c1a333d
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-20 18:11:41 +0100

    Off to find burried treasure on polygon island
    Add feature to remove islanded areas
    
    Known bugs:
    
    When dragging a via with a "solid" style thermal, a polygon piece
    will not (always?) be un-flagged as an island. The same via as part
    of a selection will re-create the polygon.

--- src/draw.c
+++ src/draw.c
@@ -1931,6 +1931,10 @@ DrawPlainPolygon (LayerTypePtr Layer, PolygonTypePtr Polygon)
   if (!Polygon->Clipped)
     return;
 
+  /* Re-use HOLEFLAG to cut out islands */
+  if (TEST_FLAG (HOLEFLAG, Polygon))
+    return;
+
   if (Gathering)
     {
       AddPart (Polygon);
