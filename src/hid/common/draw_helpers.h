9c4f1da0677fee70134ec6c5f8df56167a3e454c
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-20 17:45:16 +0100

    Refresh of hid-common-provide-a-helper-fu

--- src/hid/common/draw_helpers.h
+++ src/hid/common/draw_helpers.h
@@ -1,4 +1,5 @@
 void common_fill_pcb_polygon (hidGC gc, PolygonType *poly,
                               const BoxType *clip_box);
 void common_thindraw_pcb_polygon (hidGC gc, PolygonType *poly,
+void common_draw_helpers_init (HID *hid);
                                   const BoxType *clip_box);
