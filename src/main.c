bert/pcb

commit 2d8dc8a3a3a55158b4e6278dd9f40588e4111c2d

[PATCH] fix bug 2793480 (vias/arcs-to-polygon clearances)
frac_circle(): introduce radius_adjustment factor to make
the polygon outline the arc rather than connet points on the arc

ArcPolyNoIntersect(): compute number of segments so that
polygon diverges from the arc no more than 0.02 of
required thickness; adjust outer "arc" radius like in frac_circle()

src/polygon.c
@@ -1,10 +1,8 @@
-/* $Id$ */
-
 /*
  *                            COPYRIGHT
  *
  *  PCB, interactive printed circuit board design
- *  Copyright (C) 1994,1995,1996 Thomas Nau
+ *  Copyright (C) 1994,1995,1996,2010 Thomas Nau
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,7 +16,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *  Contact addresses for paper mail and Email:
  *  Thomas Nau, Schlehenweg 15, 88471 Baustetten, Germany
@@ -104,8 +102,6 @@ dicer output is used for HIDs which cannot render things with holes
 #include <dmalloc.h>
 #endif
 
-RCSID ("$Id$");
-
 #define ROUND(x) ((long)(((x) >= 0 ? (x) + 0.5  : (x) - 0.5)))
 
 #define UNSUBTRACT_BLOAT 10
@@ -121,6 +117,11 @@ static double circleVerticies[] = {
   1.0, 0.0,
   0.98768834059513777, 0.15643446504023087,
 };
+/* adjustment to make the segments
+ * outline the circle rather than connect
+ * points on the circle
+ * 1 - cos(\alpha/2) < (\alpha/2)^2/2 */
+static double radius_adjustment = (M_PI/CIRC_SEGS)*(M_PI/CIRC_SEGS)/2;
 
 Cardinal
 polygon_point_idx (PolygonTypePtr polygon, PointTypePtr point)
@@ -400,11 +401,11 @@ frac_circle (PLINE * c, LocationType X, LocationType Y, Vector v, int range)
 
   poly_InclVertex (c->head.prev, poly_CreateNode (v));
   /* move vector to origin */
-  e1 = v[0] - X;
-  e2 = v[1] - Y;
+  e1 = (v[0] - X) * (1 + radius_adjustment);
+  e2 = (v[1] - Y) * (1 + radius_adjustment);
 
   /* NB: the caller adds the last vertex, hence the -1 */
-  range = CIRC_SEGS / range - 1;
+  range = range == 1 ? CIRC_SEGS-1 : (CIRC_SEGS / range) - 1;
   for (i = 0; i < range; i++)
     {
       /* rotate the vector */
@@ -479,6 +480,9 @@ ArcPolyNoIntersect (ArcType * a, BDimension thick)
   int i, segs;
   double ang, da, rx, ry;
   long half;
+  static const double delta_th = 0.02; /* polygon diverges from modelled arc
+					  no more than delta_th * thick */
+  double radius_adj;
 
   if (thick <= 0)
     return NULL;
@@ -492,9 +496,15 @@ ArcPolyNoIntersect (ArcType * a, BDimension thick)
   /* start with inner radius */
   rx = MAX (a->Width - half, 0);
   ry = MAX (a->Height - half, 0);
-  segs = a->Delta / ARC_ANGLE;
+  segs = 1;
+  if(thick > 0)
+    segs = a->Delta * M_PI / 360
+	    * sqrt(sqrt((double)rx*rx + (double)ry*ry)/delta_th/2/thick);
+  segs = MAX(segs, a->Delta / ARC_ANGLE);
+
   ang = a->StartAngle;
   da = (1.0 * a->Delta) / segs;
+  radius_adj = (M_PI*da/360)*(M_PI*da/360)/2;
   v[0] = a->X - rx * cos (ang * M180);
   v[1] = a->Y + ry * sin (ang * M180);
   if ((contour = poly_NewContour (v)) == NULL)
@@ -508,13 +518,13 @@ ArcPolyNoIntersect (ArcType * a, BDimension thick)
     }
   /* find last point */
   ang = a->StartAngle + a->Delta;
-  v[0] = a->X - rx * cos (ang * M180);
-  v[1] = a->Y + ry * sin (ang * M180);
+  v[0] = a->X - rx * cos (ang * M180) * (1 - radius_adj);
+  v[1] = a->Y + ry * sin (ang * M180) * (1 - radius_adj);
   /* add the round cap at the end */
   frac_circle (contour, ends->X2, ends->Y2, v, 2);
   /* and now do the outer arc (going backwards) */
-  rx = a->Width + half;
-  ry = a->Width + half;
+  rx = (a->Width + half) * (1+radius_adj);
+  ry = (a->Width + half) * (1+radius_adj);
   da = -da;
   for (i = 0; i < segs; i++)
     {
@@ -525,8 +535,8 @@ ArcPolyNoIntersect (ArcType * a, BDimension thick)
     }
   /* now add other round cap */
   ang = a->StartAngle;
-  v[0] = a->X - rx * cos (ang * M180);
-  v[1] = a->Y + ry * sin (ang * M180);
+  v[0] = a->X - rx * cos (ang * M180) * (1 - radius_adj);
+  v[1] = a->Y + ry * sin (ang * M180) * (1 - radius_adj);
   frac_circle (contour, ends->X1, ends->Y1, v, 2);
   /* now we have the whole contour */
   if (!(np = ContourToPoly (contour)))

Nullkraft/PCB-GL

commit 007b6f4a42022b4c42c1ba47cdd9222ef97c790c

Fix geometry errors caused by commit 2d8dc8a3a3a55158b4e6278dd9f40588e4111c2d
This fixes up problems from the following commit:

    [PATCH] fix bug 2793480 (vias/arcs-to-polygon clearances)

    frac_circle(): introduce radius_adjustment factor to make
    the polygon outline the arc rather than connet points on the arc

    ArcPolyNoIntersect(): compute number of segments so that
    polygon diverges from the arc no more than 0.02 of
    required thickness; adjust outer "arc" radius like in frac_circle()

The object bounding boxes for arcs, vias, lines and rounded pads which
determine the maxiimum area affected by that object were no longer
correct, leading to artaefacts when doing incremental polygon processing.

It also lead to missing fragments in (the non-curved + and x type
thermals). These are also fixed up by this patch.

src/main.c
@@ -55,6 +55,7 @@
 #include "misc.h"
 #include "lrealpath.h"
 #include "free_atexit.h"
+#include "polygon.h"
 
 #include "hid/common/actions.h"
 
@@ -909,6 +910,7 @@ main (int argc, char *argv[])
 
   srand ( time(NULL) ); /* Set seed for rand() */
 
+  polygon_init ();
   hid_init ();
 
   hid_load_settings ();

src/misc.c
@@ -173,6 +173,10 @@ SetPinBoundingBox (PinTypePtr Pin)
    */
   width = (Pin->Clearance + Pin->Thickness + 1) / 2;
   width = MAX (width, (Pin->Mask + 1) / 2);
+
+  /* Adjust for our discrete polygon approximation */
+  width = (int)((float)width * POLY_CIRC_RADIUS_ADJ + 0.5);
+
   Pin->BoundingBox.X1 = Pin->X - width;
   Pin->BoundingBox.Y1 = Pin->Y - width;
   Pin->BoundingBox.X2 = Pin->X + width;
@@ -226,6 +230,9 @@ SetPadBoundingBox (PadTypePtr Pad)
     }
   else
     {
+      /* Adjust for our discrete polygon approximation */
+      width = (int)((float)width * POLY_CIRC_RADIUS_ADJ + 0.5);
+
       Pad->BoundingBox.X1 = MIN (Pad->Point1.X, Pad->Point2.X) - width;
       Pad->BoundingBox.X2 = MAX (Pad->Point1.X, Pad->Point2.X) + width;
       Pad->BoundingBox.Y1 = MIN (Pad->Point1.Y, Pad->Point2.Y) - width;
@@ -243,6 +250,9 @@ SetLineBoundingBox (LineTypePtr Line)
   BDimension width;
 
   width = (Line->Thickness + Line->Clearance + 1) / 2;
+
+  /* Adjust for our discrete polygon approximation */
+  width = (int)((float)width * POLY_CIRC_RADIUS_ADJ + 0.5);
 
   Line->BoundingBox.X1 = MIN (Line->Point1.X, Line->Point2.X) - width;
   Line->BoundingBox.X2 = MAX (Line->Point1.X, Line->Point2.X) + width;
@@ -1333,10 +1343,10 @@ GetObjectBoundingBox (int Type, void *Ptr1, void *Ptr2, void *Ptr3)
 void
 SetArcBoundingBox (ArcTypePtr Arc)
 {
-  register double ca1, ca2, sa1, sa2;
+  double ca1, ca2, sa1, sa2;
   double minx, maxx, miny, maxy;
-  register LocationType ang1, ang2, delta, a;
-  register LocationType width;
+  LocationType ang1, ang2, delta, a;
+  LocationType width;
 
   /* first put angles into standard form */
   if (Arc->Delta > 360)
@@ -1398,14 +1408,16 @@ SetArcBoundingBox (ArcTypePtr Arc)
     }
 
   Arc->BoundingBox.X2 = Arc->X - Arc->Width * minx;
-
   Arc->BoundingBox.X1 = Arc->X - Arc->Width * maxx;
-
   Arc->BoundingBox.Y2 = Arc->Y + Arc->Height * maxy;
-
   Arc->BoundingBox.Y1 = Arc->Y + Arc->Height * miny;
 
   width = (Arc->Thickness + Arc->Clearance) / 2;
+
+  /* Adjust for our discrete polygon approximation */
+  width = (int)((float)width * MAX (POLY_CIRC_RADIUS_ADJ,
+                                     (1.0 + POLY_ARC_MAX_DEVIATION)) + 0.5);
+
   Arc->BoundingBox.X1 -= width;
   Arc->BoundingBox.X2 += width;
   Arc->BoundingBox.Y1 -= width;

src/polygon.c
@@ -108,20 +108,17 @@ dicer output is used for HIDs which cannot render things with holes
 #define SUBTRACT_PIN_VIA_BATCH_SIZE 100
 #define SUBTRACT_LINE_BATCH_SIZE 20
 
-/* ---------------------------------------------------------------------------
- * local prototypes
- */
+static double rotate_circle_seg[4];
 
-#define CIRC_SEGS 40
-static double circleVerticies[] = {
-  1.0, 0.0,
-  0.98768834059513777, 0.15643446504023087,
-};
-/* adjustment to make the segments
- * outline the circle rather than connect
- * points on the circle
- * 1 - cos(\alpha/2) < (\alpha/2)^2/2 */
-static double radius_adjustment = (M_PI/CIRC_SEGS)*(M_PI/CIRC_SEGS)/2;
+void
+polygon_init (void)
+{
+  double cos_ang = cos (2.0 * M_PI / POLY_CIRC_SEGS_F);
+  double sin_ang = sin (2.0 * M_PI / POLY_CIRC_SEGS_F);
+
+  rotate_circle_seg[0] = cos_ang;  rotate_circle_seg[1] = -sin_ang;
+  rotate_circle_seg[2] = sin_ang;  rotate_circle_seg[3] =  cos_ang;
+}
 
 Cardinal
 polygon_point_idx (PolygonTypePtr polygon, PointTypePtr point)
@@ -401,16 +398,16 @@ frac_circle (PLINE * c, LocationType X, LocationType Y, Vector v, int range)
 
   poly_InclVertex (c->head.prev, poly_CreateNode (v));
   /* move vector to origin */
-  e1 = (v[0] - X) * (1 + radius_adjustment);
-  e2 = (v[1] - Y) * (1 + radius_adjustment);
+  e1 = (v[0] - X) * POLY_CIRC_RADIUS_ADJ;
+  e2 = (v[1] - Y) * POLY_CIRC_RADIUS_ADJ;
 
   /* NB: the caller adds the last vertex, hence the -1 */
-  range = CIRC_SEGS / range - 1;
+  range = POLY_CIRC_SEGS / range - 1;
   for (i = 0; i < range; i++)
     {
       /* rotate the vector */
-      t1 = e1 * circleVerticies[2] - e2 * circleVerticies[3];
-      e2 = e1 * circleVerticies[3] + e2 * circleVerticies[2];
+      t1 = rotate_circle_seg[0] * e1 + rotate_circle_seg[1] * e2;
+      e2 = rotate_circle_seg[2] * e1 + rotate_circle_seg[3] * e2;
       e1 = t1;
       v[0] = X + ROUND (e1);
       v[1] = Y + ROUND (e2);
@@ -480,8 +477,6 @@ ArcPolyNoIntersect (ArcType * a, BDimension thick)
   int i, segs;
   double ang, da, rx, ry;
   long half;
-  static const double delta_th = 0.02; /* polygon diverges from modelled arc
-					  no more than delta_th * thick */
   double radius_adj;
 
   if (thick <= 0)
@@ -498,8 +493,9 @@ ArcPolyNoIntersect (ArcType * a, BDimension thick)
   ry = MAX (a->Height - half, 0);
   segs = 1;
   if(thick > 0)
-    segs = MAX (segs, a->Delta * M_PI / 360
-		* sqrt(sqrt((double)rx*rx + (double)ry*ry)/delta_th/2/thick));
+    segs = MAX (segs, a->Delta * M_PI / 360 *
+                      sqrt (sqrt ((double)rx * rx + (double)ry * ry) /
+                            POLY_ARC_MAX_DEVIATION / 2 / thick));
   segs = MAX(segs, a->Delta / ARC_ANGLE);
 
   ang = a->StartAngle;

src/polygon.h
@@ -33,6 +33,23 @@
 
 #include "global.h"
 
+/* Implementation constants */
+
+#define POLY_CIRC_SEGS 40
+#define POLY_CIRC_SEGS_F ((float)POLY_CIRC_SEGS)
+
+/* adjustment to make the segments outline the circle rather than connect
+ * points on the circle: 1 - cos (\alpha / 2) < (\alpha / 2) ^ 2 / 2
+ */
+#define POLY_CIRC_RADIUS_ADJ (1.0 + M_PI / POLY_CIRC_SEGS_F * \
+                                    M_PI / POLY_CIRC_SEGS_F / 2.0)
+
+/* polygon diverges from modelled arc no more than MAX_ARC_DEVIATION * thick */
+#define POLY_ARC_MAX_DEVIATION 0.02
+
+/* Prototypes */
+
+void polygon_init (void);
 Cardinal polygon_point_idx (PolygonTypePtr polygon, PointTypePtr point);
 Cardinal polygon_point_contour (PolygonTypePtr polygon, Cardinal point);
 Cardinal prev_contour_point (PolygonTypePtr polygon, Cardinal point);

src/thermal.c
@@ -436,15 +436,21 @@ ThermPoly (PCBTypePtr p, PinTypePtr pin, Cardinal laynum)
         /* fix me needs error checking */
         if (style == 2)
           {
-            pa = RectPoly (pin->X - t, pin->X + t, pin->Y - w, pin->Y + w);
+            /* t is the theoretically required length, but we use twice that
+             * to avoid descritisation errors in our circle approximation.
+             */
+            pa = RectPoly (pin->X - t * 2, pin->X + t * 2, pin->Y - w, pin->Y + w);
             poly_Boolean_free (m, pa, &arc, PBO_SUB);
-            pa = RectPoly (pin->X - w, pin->X + w, pin->Y - t, pin->Y + t);
+            pa = RectPoly (pin->X - w, pin->X + w, pin->Y - t * 2, pin->Y + t * 2);
           }
         else
           {
-            pa = diag_line (pin->X, pin->Y, t, w, true);
+            /* t is the theoretically required length, but we use twice that
+             * to avoid descritisation errors in our circle approximation.
+             */
+            pa = diag_line (pin->X, pin->Y, t * 2, w, true);
             poly_Boolean_free (m, pa, &arc, PBO_SUB);
-            pa = diag_line (pin->X, pin->Y, t, w, false);
+            pa = diag_line (pin->X, pin->Y, t * 2, w, false);
           }
         poly_Boolean_free (arc, pa, &m, PBO_SUB);
         return m;