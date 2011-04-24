a417d7600bfee208f167d26c9de7b66bca8ef9d6
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-24 19:33:03 +0100

    Add strip hierarchy options to display refdes without any "..../" prefix

--- src/draw.c
+++ src/draw.c
@@ -1700,6 +1700,32 @@ DrawElement (ElementTypePtr Element)
   DrawElementPinsAndPads (Element);
 }
 
+static void
+DrawStrippedText (ElementTypePtr Element, int min_width)
+{
+  TextType text;
+  TextType *text_ptr;
+  char *end_string;
+
+  if (TEST_FLAG (NAMEONPCBFLAG, PCB) &&
+      TEST_FLAG (STRIPHIERFLAG, Element))
+    {
+      text_ptr = &text;
+      memcpy (text_ptr, &ELEMENT_TEXT (PCB, Element), sizeof (TextType));
+
+      /* Strip hierarchy */
+      end_string = strrchr (text.TextString, '/');
+      if (end_string != NULL)
+        text.TextString = end_string + 1;
+    }
+  else
+    {
+      text_ptr = &ELEMENT_TEXT (PCB, Element);
+    }
+
+  DrawTextLowLevel (text_ptr, min_width);
+}
+
 /* ---------------------------------------------------------------------------
  * draws the name of an element
  */
@@ -1718,7 +1744,7 @@ DrawElementName (ElementTypePtr Element)
     gui->set_color (Output.fgGC, PCB->ElementColor);
   else
     gui->set_color (Output.fgGC, PCB->InvisibleObjectsColor);
-  DrawTextLowLevel (&ELEMENT_TEXT (PCB, Element), PCB->minSlk);
+  DrawStrippedText (Element, PCB->minSlk);
 }
 
 /* ---------------------------------------------------------------------------
@@ -1911,7 +1937,7 @@ EraseElement (ElementTypePtr Element)
   }
   END_LOOP;
   if (!TEST_FLAG (HIDENAMEFLAG, Element))
-    DrawTextLowLevel (&ELEMENT_TEXT (PCB, Element), PCB->minSlk);
+    DrawStrippedText (Element, PCB->minSlk);
   EraseElementPinsAndPads (Element);
 }
 
@@ -1945,7 +1971,7 @@ EraseElementName (ElementTypePtr Element)
 {
   if (TEST_FLAG (HIDENAMEFLAG, Element))
     return;
-  DrawTextLowLevel (&ELEMENT_TEXT (PCB, Element), PCB->minSlk);
+  DrawStrippedText (Element, PCB->minSlk);
 }
 
 
