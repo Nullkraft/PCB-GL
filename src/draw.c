bc703a53bce73491089f0937d6ea9cfc5d908a5e
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-26 00:10:40 +0100

    draw.c: Split "Gathering" from real drawing routines (Element Pins and Pads)

--- src/draw.c
+++ src/draw.c
@@ -1562,6 +1562,8 @@ DrawElementPackage (ElementTypePtr Element)
 void
 DrawElementPinsAndPads (ElementTypePtr Element)
 {
+  assert (Gathering);
+
   PAD_LOOP (Element);
   {
     if (doing_pinout || doing_assy || FRONT (pad) || PCB->InvisibleObjectsOn)
@@ -1571,6 +1573,22 @@ DrawElementPinsAndPads (ElementTypePtr Element)
   PIN_LOOP (Element);
   {
     DrawPin (pin);
+  }
+  END_LOOP;
+}
+
+static void
+draw_element_pins_and_pads (ElementType *element)
+{
+  PAD_LOOP (element);
+  {
+    if (doing_pinout || doing_assy || FRONT (pad) || PCB->InvisibleObjectsOn)
+      DrawPad (pad);
+  }
+  END_LOOP;
+  PIN_LOOP (element);
+  {
+    DrawPin (pin, true);
   }
   END_LOOP;
 }
@@ -1851,7 +1869,7 @@ draw_element (ElementTypePtr element)
 {
   draw_element_package (element);
   draw_element_name (element);
-  DrawElementPinsAndPads (element);
+  draw_element_pins_and_pads (element);
 }
 
 /* ---------------------------------------------------------------------------
