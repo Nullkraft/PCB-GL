d24fb3f59695bf3d8821e10b67a6b996b33fc1b4
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-25 14:07:48 +0100

    Add support for a "poured" object.
    This takes half of the existing polygon functionality, and splits it
    such that the user defines a poured region (similar to drawing a polygon
    previously), and after clipping, this produces zero or more polygon
    objects which are drawn and connectivity checked with the existing
    polygon code.
    
    Selecting a "polygon" will cause the parent "pour" to be looked up, and
    all child polygons selected. Manipulations will always occur on the
    "pour", with "polygons" being updated after clipping.
    
    It remains to be seen whether "pour" will have its own file-format entry,
    or if it will take over the meaning of the existing "polygon" entry.

--- src/rotate.h
+++ src/rotate.h
@@ -64,7 +64,7 @@ void RotateLineLowLevel (LineTypePtr, LocationType, LocationType, BYTE);
 void RotateArcLowLevel (ArcTypePtr, LocationType, LocationType, BYTE);
 void RotateBoxLowLevel (BoxTypePtr, LocationType, LocationType, BYTE);
 void RotateTextLowLevel (TextTypePtr, LocationType, LocationType, BYTE);
-void RotatePolygonLowLevel (PolygonTypePtr, LocationType, LocationType, BYTE);
+void RotatePourLowLevel (PourTypePtr, LocationType, LocationType, BYTE);
 void RotateElementLowLevel (DataTypePtr, ElementTypePtr, LocationType,
 			    LocationType, BYTE);
 void *RotateObject (int, void *, void *, void *, LocationType, LocationType,
