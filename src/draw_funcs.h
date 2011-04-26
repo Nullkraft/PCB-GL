2b7ccb1797ded5bf38f6107c5359beb63000a872
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-26 18:54:36 +0100

    Refresh of add-support-for-a-poured-objec-I

--- src/draw_funcs.h
+++ src/draw_funcs.h
@@ -11,6 +11,7 @@ struct draw_funcs {
   void (*draw_rat)       (RatType *,     void *);
   void (*draw_arc)       (ArcType *,     void *);
   void (*draw_poly)      (PolygonType *, void *);
+  void (*draw_pour)      (PourType *,    void *);
   void (*draw_layer)     (LayerType *,   void *);
 };
 
