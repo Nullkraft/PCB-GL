26ac5a3021e2154c94b91a0b32fed7b21db969dd
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-26 14:17:03 +0100

    Refresh of crasy-test

---
+++ src/draw_funcs.h
@@ -0,0 +1,15 @@
+struct draw_funcs {
+  void (*draw_pin)       (PinType *,     void *);
+  void (*draw_pin_mask)  (PinType *,     void *);
+  void (*draw_via)       (PinType *,     void *);
+  void (*draw_via_mask)  (PinType *,     void *);
+  void (*draw_pad)       (PadType *,     void *);
+  void (*draw_pad_mask)  (PadType *,     void *);
+  void (*draw_pad_paste) (PadType *,     void *);
+  void (*draw_line)      (LineType *,    void *);
+  void (*draw_rat)       (RatType *,     void *);
+  void (*draw_arc)       (ArcType *,     void *);
+  void (*draw_poly)      (PolygonType *, void *);
+};
+
+extern struct draw_funcs *dapi;
