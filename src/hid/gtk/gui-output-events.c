ebf7380e41a5f28f4b95da48dbf58ee3a644ba1a
Peter Clifton <pcjc2@cam.ac.uk>
2011-04-27 20:23:36 +0100

    TEMPORARY: Comment out some calls which can trigger an out-of-context redraw
    Comment out some DrawMark calls which might be out of context
    Comment out some crosshair drawing calls which might be called out of context
    
    Should fix this properly with notificatons to the GUI

--- src/hid/gtk/gui-output-events.c
+++ src/hid/gtk/gui-output-events.c
@@ -335,7 +335,7 @@ ghid_port_key_press_cb (GtkWidget * drawing_area,
   state = (GdkModifierType) (kev->state);
   mk = ghid_modifier_keys_state (&state);
 
-  ghid_show_crosshair (FALSE);
+  //ghid_show_crosshair (FALSE);
 
   handled = TRUE;		/* Start off assuming we handle it */
   switch (ksym)
@@ -440,7 +440,7 @@ ghid_port_button_press_cb (GtkWidget * drawing_area,
   ghid_note_event_location (ev);
   state = (GdkModifierType) (ev->state);
   mk = ghid_modifier_keys_state (&state);
-  ghid_show_crosshair (FALSE);
+  //ghid_show_crosshair (FALSE);
 
   do_mouse_action(ev->button, mk);
 
@@ -795,7 +795,7 @@ ghid_port_window_leave_cb (GtkWidget * widget,
 	}
     }
 
-  ghid_show_crosshair (FALSE);
+  //ghid_show_crosshair (FALSE);
   out->has_entered = FALSE;
 
   ghid_screen_update ();
