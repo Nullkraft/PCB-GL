48dd469df334a5eb4d764fafdf041307b554bf19
Peter Clifton <pcjc2@cam.ac.uk>
2011-05-02 14:49:21 +0100

    Crazy some more

--- src/draw_funcs.h
+++ src/draw_funcs.h
@@ -12,6 +12,8 @@ struct draw_funcs {
   void (*draw_rat)       (RatType *,     const BoxType *, void *);
   void (*draw_arc)       (ArcType *,     const BoxType *, void *);
   void (*draw_poly)      (PolygonType *, const BoxType *, void *);
+  void (*draw_ppv)       (int,           const BoxType *, void *);
+  void (*draw_holes)     (int,           const BoxType *, void *);
   void (*draw_layer)     (LayerType *,   const BoxType *, void *);
 };
 
