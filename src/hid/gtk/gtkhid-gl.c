81a24341a8eff666d1dd7d79d2f7a50916ef93b6
Peter Clifton <pcjc2@cam.ac.uk>
2011-05-05 21:05:50 +0100

    hid/gtk: Add feature to force an immediate redraw if framerate is low
    If more than a certain time (currently hard-coded at 50ms) has elapsed
    since the last expose callback, invalidate_all() will wait and process
    updates before returning.
    
    This is handy to allow animation of auto-router progress, where the gui
    mainloop is not hit until the auto-routing operation is fully complete.

--- src/hid/gtk/gtkhid-gl.c
+++ src/hid/gtk/gtkhid-gl.c
@@ -45,6 +45,7 @@ typedef struct render_priv {
   bool trans_lines;
   bool in_context;
   int subcomposite_stencil_bit;
+  GTimer *time_since_expose;
 } render_priv;
 
 
@@ -570,10 +571,17 @@ ghid_invalidate_lr (int left, int right, int top, int bottom)
   ghid_invalidate_all ();
 }
 
+#define MAX_ELAPSED (50. / 1000.) /* 50ms */
 void
 ghid_invalidate_all ()
 {
+  render_priv *priv = gport->render_priv;
+  double elapsed = g_timer_elapsed (priv->time_since_expose, NULL);
+
   ghid_draw_area_update (gport, NULL);
+
+  if (elapsed > MAX_ELAPSED)
+    gdk_window_process_all_updates ();
 }
 
 void
@@ -771,6 +779,8 @@ ghid_init_renderer (int *argc, char ***argv, GHidPort *port)
 
   port->render_priv = priv = g_new0 (render_priv, 1);
 
+  priv->time_since_expose = g_timer_new ();
+
   gtk_gl_init(argc, argv);
 
   /* setup GL-context */
@@ -848,6 +858,7 @@ ghid_drawing_area_expose_cb (GtkWidget *widget,
                              GdkEventExpose *ev,
                              GHidPort *port)
 {
+  render_priv *priv = port->render_priv;
   BoxType region;
 
   ghid_start_drawing (port);
@@ -938,6 +949,8 @@ ghid_drawing_area_expose_cb (GtkWidget *widget,
   hidgl_flush_triangles (&buffer);
 
   ghid_end_drawing (port);
+
+  g_timer_start (priv->time_since_expose);
 
   return FALSE;
 }
