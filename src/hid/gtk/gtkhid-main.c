bfbbcca3f5d610ad865395dea92c32d6aa2641d4
Peter Clifton <pcjc2@cam.ac.uk>
2011-08-28 01:26:19 +0100

    Remove thindraw_pcb_polygon from the HID API
    Just pass call the same function for all polygons, let the HID decide.

--- src/hid/gtk/gtkhid-main.c
+++ src/hid/gtk/gtkhid-main.c
@@ -2149,7 +2149,6 @@ hid_gtk_init ()
   ghid_hid.draw_arc                 = ghid_draw_arc;
   ghid_hid.draw_rect                = ghid_draw_rect;
   ghid_hid.fill_circle              = ghid_fill_circle;
-  ghid_hid.fill_polygon             = ghid_fill_polygon;
   ghid_hid.fill_rect                = ghid_fill_rect;
 
   ghid_hid.calibrate                = ghid_calibrate;
