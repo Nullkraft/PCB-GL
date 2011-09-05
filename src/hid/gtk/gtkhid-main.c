6542d10622a45f2d8b29794f8a6ad03944e52b32
Peter Clifton <pcjc2@cam.ac.uk>
2011-09-05 22:18:16 +0100

    hid/gtk: Connect up the file-change monitor in a more sensible place
    I must have had a brain-fail when I hooked it up in the call which sets
    the window title. NB: That also gets called after every menu operation!
    
    Hook up the connection in the ghid_sync_with_new_layout() function,
    which looks to be a much more appropriate place, however changing this
    alone revealed another issue - we now get notified for changes WE make
    to the files.
    
    We were avoiding those events as the file-monitor was being reset before
    it could pop up, at the end of the menu action which invoked the save).
    
    Hook up to the new HID API (added in the previous commit) which allows
    us to temporarily disconnect the file-monitor when PCB's core is saving
    to the PCB file.

--- src/hid/gtk/gtkhid-main.c
+++ src/hid/gtk/gtkhid-main.c
@@ -2169,6 +2169,8 @@ hid_gtk_init ()
   ghid_hid.flush_debug_draw         = ghid_flush_debug_draw;
   ghid_hid.finish_debug_draw        = ghid_finish_debug_draw;
 
+  ghid_hid.notify_save_pcb          = ghid_notify_save_pcb;
+
   hid_register_hid (&ghid_hid);
 #include "gtk_lists.h"
 }
