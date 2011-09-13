fcb2b3e11115bf44bc961148a20357984113f611
Peter Clifton <pcjc2@cam.ac.uk>
2011-09-13 19:18:26 +0100

    Bentley-Ottann test implementation
    Code from cairo - intersection routines stripped out

---
+++ src/sweep.h
@@ -0,0 +1,4 @@
+#include "borast/borast-traps-private.h"
+
+borast_status_t bo_poly_to_traps (POLYAREA *poly, borast_traps_t *traps);
+borast_status_t bo_contour_to_traps (PLINE *contour, borast_traps_t *traps);
