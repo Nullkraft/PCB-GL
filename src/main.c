915f970c850d066e8c7206b99592b1ecd9ce578d
Peter Clifton <pcjc2@cam.ac.uk>
2011-09-23 01:09:13 +0100

    TMP2

--- src/main.c
+++ src/main.c
@@ -57,6 +57,7 @@
 #include "free_atexit.h"
 #include "polygon.h"
 #include "pcb-printf.h"
+#include "setjmp.h"
 #include "sweep.h"
 
 #include "hid/common/actions.h"
@@ -1963,7 +1964,7 @@ main (int argc, char *argv[])
   // bos_test_intersections ();
   my_cairo_test ();
 
-  // gui->do_export (0);
+  gui->do_export (0);
 
 #if HAVE_DBUS
   pcb_dbus_finish();
