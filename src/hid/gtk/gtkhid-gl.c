db7732fd01d10cd7c40e2f90d99297f84379baf6
Peter Clifton <pcjc2@cam.ac.uk>
2011-09-29 16:18:58 +0100

    Play with GLSL shader for rendering line caps and circles
    And of course.. typically, a million other little fixes:
    
     - Primitive rendering object changed from GL_TRIANGLES to GL_TRIANGLE_STRIP
     - Cache of tristrip data per contour (HACK: in the core)
     - Rendering using mapped VBOs rather than just arrays
     - Updated layer depth code, so we render with "z" in world coordinates
     - Space navigator joystick support fixes against later kernels
    
    Bugs:
     - Layer depths will be wrong for reversed layer group numbering
     - Pixel shader ought to be switched off for polygon fill?
     - hidgl_draw_rect stubbed out (for benchmarking)
     - Draw Element mark stubbed out (for benchmarking)
     - Draw crosshair stubbed out (for benchmarking)
     - Draw crosshair not updated to render in world coordinates
    
    Notes:
     - Apparently it can be better to do glVertexPointer last (so we do):
        From: http://sdickinson.com/wordpress/?p=122
        ""
        Make sure you put glVertexPointer at the end as there is a lot of work that
        goes on behind the scenes with it, and if it's set at the start, it has to
        do all that work for each gl*Pointer call, rather than once at the end.
        ""
    
     - We set up the array pointers before calling glEnableClientState():
        I'm not sure, but if we do this the other way around, it might cause
        the driver to hang onto our old buffers longer, or perform state setup,
        only to have to discard that work shortly after.
    
     - DARN: Seems that using VBOs is not a win, even on Intel 965:
        Certainly seems to fail miserably (v. low FPS) on my NVidia card.
    
        On Intel, sysprof shows a huge amount of time spent clflushing
        buffers, or copying to the graphics card (uncached).
    
        Set to use glBufferSubData by default for now.

--- src/hid/gtk/gtkhid-gl.c
+++ src/hid/gtk/gtkhid-gl.c
@@ -319,6 +319,8 @@ ghid_draw_grid (BoxTypePtr drawn_area)
       gport->grid_color.blue ^= gport->bg_color.blue;
     }
 
+  glTexCoord2f (0., 0.);
+
   glDisable (GL_STENCIL_TEST);
   glEnable (GL_COLOR_LOGIC_OP);
   glLogicOp (GL_XOR);
@@ -691,7 +693,7 @@ ghid_fill_circle (hidGC gc, Coord cx, Coord cy, Coord radius)
 {
   USE_GC (gc);
 
-  hidgl_fill_circle (cx, cy, radius, gport->view.coord_per_px);
+  hidgl_fill_circle (cx, cy, radius);
 }
 
 
@@ -708,7 +710,7 @@ ghid_fill_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box)
 {
   USE_GC (gc);
 
-  hidgl_fill_pcb_polygon (poly, clip_box, gport->view.coord_per_px);
+  hidgl_fill_pcb_polygon (poly, clip_box);
 }
 
 void
@@ -1665,9 +1667,7 @@ DrawDrillChannel (int vx, int vy, int vr, int from_layer, int to_layer, double s
 #define MIN_FACES_PER_CYL 6
 #define MAX_FACES_PER_CYL 360
   float radius = vr;
-  float x1, y1;
-  float x2, y2;
-  float z1, z2;
+  float x, y, z1, z2;
   int i;
   int slices;
 
@@ -1682,19 +1682,27 @@ DrawDrillChannel (int vx, int vy, int vr, int from_layer, int to_layer, double s
   z1 = compute_depth (from_layer);
   z2 = compute_depth (to_layer);
 
-  x1 = vx + vr;
-  y1 = vy;
+  x = vx + vr;
+  y = vy;
 
-  hidgl_ensure_triangle_space (&buffer, 2 * slices);
+  hidgl_ensure_vertex_space (&buffer, 2 * slices + 2 + 2);
+
+  /* NB: Repeated first virtex to separate from other tri-strip */
+  hidgl_add_vertex_3D_tex (&buffer, x, y, z1, 0.0, 0.0);
+  hidgl_add_vertex_3D_tex (&buffer, x, y, z1, 0.0, 0.0);
+  hidgl_add_vertex_3D_tex (&buffer, x, y, z2, 0.0, 0.0);
+
   for (i = 0; i < slices; i++)
     {
-      x2 = radius * cosf (((float)(i + 1)) * 2. * M_PI / (float)slices) + vx;
-      y2 = radius * sinf (((float)(i + 1)) * 2. * M_PI / (float)slices) + vy;
-      hidgl_add_triangle_3D (&buffer, x1, y1, z1,  x2, y2, z1,  x1, y1, z2);
-      hidgl_add_triangle_3D (&buffer, x2, y2, z1,  x1, y1, z2,  x2, y2, z2);
-      x1 = x2;
-      y1 = y2;
+      x = radius * cosf (((float)(i + 1)) * 2. * M_PI / (float)slices) + vx;
+      y = radius * sinf (((float)(i + 1)) * 2. * M_PI / (float)slices) + vy;
+
+      hidgl_add_vertex_3D_tex (&buffer, x, y, z1, 0.0, 0.0);
+      hidgl_add_vertex_3D_tex (&buffer, x, y, z2, 0.0, 0.0);
     }
+
+  /* NB: Repeated last virtex to separate from other tri-strip */
+  hidgl_add_vertex_3D_tex (&buffer, x, y, z2, 0.0, 0.0);
 }
 
 struct cyl_info {
@@ -2111,6 +2119,8 @@ ghid_drawing_area_expose_cb (GtkWidget *widget,
 
   draw_lead_user (priv);
 
+  hidgl_finish_triangle_array (&buffer);
+
   check_gl_drawing_ok_hack = false;
   hidgl_in_context (false);
   ghid_end_drawing (port);
@@ -2204,7 +2214,6 @@ ghid_pinout_preview_expose (GtkWidget *widget,
   glStencilMask (~0);
   glClearStencil (0);
   glClear (GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
   hidgl_reset_stencil_usage ();
 
   /* call the drawing routine */
