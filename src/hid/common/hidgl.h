a51b1e2535c088a2b5e0e3dfe5ae02975b1a70f8
Peter Clifton <pcjc2@cam.ac.uk>
2011-10-03 23:13:17 +0100

    Play with GLSL shader for rendering line caps and circles
    And of course.. typically, a million other little fixes:
    
     - Primitive rendering object changed from GL_TRIANGLES to GL_TRIANGLE_STRIP
     - Cache of tristrip data per contour (HACK: in the core)
     - Rendering using mapped VBOs rather than just arrays
     - Updated layer depth code, so we render with "z" in world coordinates
     - Space navigator joystick support fixes against later kernels
    
    Bugs:
     - Layer depths will be wrong for reversed layer group numbering
     - Pixel shader ought to be switched off for polygon fill?
     - hidgl_draw_rect stubbed out (for benchmarking)
     - Draw Element mark stubbed out (for benchmarking)
     - Draw crosshair stubbed out (for benchmarking)
     - Draw crosshair not updated to render in world coordinates
    
    Notes:
     - Apparently it can be better to do glVertexPointer last (so we do):
        From: http://sdickinson.com/wordpress/?p=122
        ""
        Make sure you put glVertexPointer at the end as there is a lot of work that
        goes on behind the scenes with it, and if it's set at the start, it has to
        do all that work for each gl*Pointer call, rather than once at the end.
        ""
    
     - We set up the array pointers before calling glEnableClientState():
        I'm not sure, but if we do this the other way around, it might cause
        the driver to hang onto our old buffers longer, or perform state setup,
        only to have to discard that work shortly after.
    
     - DARN: Seems that using VBOs is not a win, even on Intel 965:
        Certainly seems to fail miserably (v. low FPS) on my NVidia card.
    
        On Intel, sysprof shows a huge amount of time spent clflushing
        buffers, or copying to the graphics card (uncached).
    
        Set to use glBufferSubData by default for now.

--- src/hid/common/hidgl.h
+++ src/hid/common/hidgl.h
@@ -23,18 +23,64 @@
 #ifndef PCB_HID_COMMON_HIDGL_H
 #define PCB_HID_COMMON_HIDGL_H
 
-#define TRIANGLE_ARRAY_SIZE 5461
+#include "hidgl_shaders.h"
+
+#define TRIANGLE_ARRAY_SIZE 30000
 typedef struct {
-  GLfloat triangle_array [3 * 3 * TRIANGLE_ARRAY_SIZE];
+  GLfloat *triangle_array;
   unsigned int triangle_count;
   unsigned int coord_comp_count;
+  unsigned int vertex_count;
+  GLuint vbo_id;
+  bool use_vbo;
+  bool use_map;
 } triangle_buffer;
 
 extern triangle_buffer buffer;
 extern float global_depth;
 
+extern hidgl_shader *circular_program;
+
 void hidgl_flush_triangles (triangle_buffer *buffer);
+void hidgl_ensure_vertex_space (triangle_buffer *buffer, int count);
 void hidgl_ensure_triangle_space (triangle_buffer *buffer, int count);
+
+static inline void
+hidgl_add_vertex_3D_tex (triangle_buffer *buffer,
+                         GLfloat x, GLfloat y, GLfloat z,
+                         GLfloat s, GLfloat t)
+{
+  buffer->triangle_array [buffer->coord_comp_count++] = x;
+  buffer->triangle_array [buffer->coord_comp_count++] = y;
+  buffer->triangle_array [buffer->coord_comp_count++] = z;
+  buffer->triangle_array [buffer->coord_comp_count++] = s;
+  buffer->triangle_array [buffer->coord_comp_count++] = t;
+  buffer->vertex_count++;
+}
+
+static inline void
+hidgl_add_vertex_tex (triangle_buffer *buffer,
+                      GLfloat x, GLfloat y,
+                      GLfloat s, GLfloat t)
+{
+  hidgl_add_vertex_3D_tex (buffer, x, y, global_depth, s, t);
+}
+
+
+static inline void
+hidgl_add_triangle_3D_tex (triangle_buffer *buffer,
+                           GLfloat x1, GLfloat y1, GLfloat z1, GLfloat s1, GLfloat t1,
+                           GLfloat x2, GLfloat y2, GLfloat z2, GLfloat s2, GLfloat t2,
+                           GLfloat x3, GLfloat y3, GLfloat z3, GLfloat s3, GLfloat t3)
+{
+  /* NB: Repeated first virtex to separate from other tri-strip */
+  hidgl_add_vertex_3D_tex (buffer, x1, y1, z1, s1, t1);
+  hidgl_add_vertex_3D_tex (buffer, x1, y1, z1, s1, t1);
+  hidgl_add_vertex_3D_tex (buffer, x2, y2, z2, s2, t2);
+  hidgl_add_vertex_3D_tex (buffer, x3, y3, z3, s3, t3);
+  hidgl_add_vertex_3D_tex (buffer, x3, y3, z3, s3, t3);
+  /* NB: Repeated last virtex to separate from other tri-strip */
+}
 
 static inline void
 hidgl_add_triangle_3D (triangle_buffer *buffer,
@@ -42,16 +88,20 @@ hidgl_add_triangle_3D (triangle_buffer *buffer,
                        GLfloat x2, GLfloat y2, GLfloat z2,
                        GLfloat x3, GLfloat y3, GLfloat z3)
 {
-  buffer->triangle_array [buffer->coord_comp_count++] = x1;
-  buffer->triangle_array [buffer->coord_comp_count++] = y1;
-  buffer->triangle_array [buffer->coord_comp_count++] = z1;
-  buffer->triangle_array [buffer->coord_comp_count++] = x2;
-  buffer->triangle_array [buffer->coord_comp_count++] = y2;
-  buffer->triangle_array [buffer->coord_comp_count++] = z2;
-  buffer->triangle_array [buffer->coord_comp_count++] = x3;
-  buffer->triangle_array [buffer->coord_comp_count++] = y3;
-  buffer->triangle_array [buffer->coord_comp_count++] = z3;
-  buffer->triangle_count++;
+  hidgl_add_triangle_3D_tex (buffer, x1, y1, z1, 0., 0.,
+                                     x2, y2, z2, 0., 0.,
+                                     x3, y3, z3, 0., 0.);
+}
+
+static inline void
+hidgl_add_triangle_tex (triangle_buffer *buffer,
+                        GLfloat x1, GLfloat y1, GLfloat s1, GLfloat t1,
+                        GLfloat x2, GLfloat y2, GLfloat s2, GLfloat t2,
+                        GLfloat x3, GLfloat y3, GLfloat s3, GLfloat t3)
+{
+  hidgl_add_triangle_3D_tex (buffer, x1, y1, global_depth, s1, t1,
+                                     x2, y2, global_depth, s2, t2,
+                                     x3, y3, global_depth, s3, t3);
 }
 
 static inline void
@@ -70,9 +120,9 @@ void hidgl_set_depth (float depth);
 void hidgl_draw_line (int cap, Coord width, Coord x1, Coord y1, Coord x2, Coord y2, double scale);
 void hidgl_draw_arc (Coord width, Coord vx, Coord vy, Coord vrx, Coord vry, Angle start_angle, Angle delta_angle, double scale);
 void hidgl_draw_rect (Coord x1, Coord y1, Coord x2, Coord y2);
-void hidgl_fill_circle (Coord vx, Coord vy, Coord vr, double scale);
+void hidgl_fill_circle (Coord vx, Coord vy, Coord vr);
 void hidgl_fill_polygon (int n_coords, Coord *x, Coord *y);
-void hidgl_fill_pcb_polygon (PolygonType *poly, const BoxType *clip_box, double scale);
+void hidgl_fill_pcb_polygon (PolygonType *poly, const BoxType *clip_box);
 void hidgl_fill_rect (Coord x1, Coord y1, Coord x2, Coord y2);
 
 void hidgl_init (void);
