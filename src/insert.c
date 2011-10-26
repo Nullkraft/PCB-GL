bert/pcb

commit b59ab044afe3ac362f8cb4da10ea410280b4323a

Audit copy.[ch], insert.[ch], mirror.[ch], implement Coord

src/insert.c
@@ -71,8 +71,7 @@ static void *InsertPointIntoRat (RatTypePtr);
 /* ---------------------------------------------------------------------------
  * some local identifiers
  */
-static LocationType InsertX,	/* used by local routines as offset */
-  InsertY;
+static Coord InsertX, InsertY;	/* used by local routines as offset */
 static Cardinal InsertAt;
 static bool InsertLast;
 static bool Forcible;
@@ -127,7 +126,7 @@ static void *
 InsertPointIntoLine (LayerTypePtr Layer, LineTypePtr Line)
 {
   LineTypePtr line;
-  LocationType X, Y;
+  Coord X, Y;
 
   if (((Line->Point1.X == InsertX) && (Line->Point1.Y == InsertY)) ||
       ((Line->Point2.X == InsertX) && (Line->Point2.Y == InsertY)))
@@ -219,7 +218,7 @@ InsertPointIntoPolygon (LayerTypePtr Layer, PolygonTypePtr Polygon)
  */
 void *
 InsertPointIntoObject (int Type, void *Ptr1, void *Ptr2, Cardinal * Ptr3,
-		       LocationType DX, LocationType DY, bool Force,
+		       Coord DX, Coord DY, bool Force,
 		       bool insert_last)
 {
   void *ptr;
@@ -245,8 +244,8 @@ PointTypePtr
 AdjustInsertPoint (void)
 {
   static PointType InsertedPoint;
-  float m;
-  LocationType x, y, dx, dy, m1, m2;
+  double m;
+  Coord x, y, m1, m2;
   LineTypePtr line = (LineTypePtr) Crosshair.AttachedObject.Ptr2;
 
   if (Crosshair.AttachedObject.State == STATE_FIRST)
@@ -255,13 +254,9 @@ AdjustInsertPoint (void)
   if (gui->shift_is_pressed ())
     {
       AttachedLineType myline;
-      dx = Crosshair.X - line->Point1.X;
-      dy = Crosshair.Y - line->Point1.Y;
-      m = dx * dx + dy * dy;
-      dx = Crosshair.X - line->Point2.X;
-      dy = Crosshair.Y - line->Point2.Y;
       /* only force 45 degree for nearest point */
-      if (m < (dx * dx + dy * dy))
+      if (Distance (Crosshair.X, Crosshair.Y, line->Point1.X, line->Point1.Y) <
+          Distance (Crosshair.X, Crosshair.Y, line->Point2.X, line->Point2.Y))
 	myline.Point1 = myline.Point2 = line->Point1;
       else
 	myline.Point1 = myline.Point2 = line->Point2;
@@ -276,26 +271,22 @@ AdjustInsertPoint (void)
       InsertedPoint.Y = Crosshair.Y;
       return &InsertedPoint;
     }
-  dx = Crosshair.X - line->Point1.X;
-  dy = Crosshair.Y - line->Point1.Y;
-  if (!dx)
+  if (Crosshair.X == line->Point1.X)
     m1 = 2;			/* 2 signals infinite slope */
   else
     {
-      m = (float) dy / (float) dx;
+      m = (double) (Crosshair.X - line->Point1.X) / (Crosshair.Y - line->Point1.Y);
       m1 = 0;
       if (m > TAN_30_DEGREE)
 	m1 = (m > TAN_60_DEGREE) ? 2 : 1;
       else if (m < -TAN_30_DEGREE)
 	m1 = (m < -TAN_60_DEGREE) ? 2 : -1;
     }
-  dx = Crosshair.X - line->Point2.X;
-  dy = Crosshair.Y - line->Point2.Y;
-  if (!dx)
+  if (Crosshair.X == line->Point2.X)
     m2 = 2;			/* 2 signals infinite slope */
   else
     {
-      m = (float) dy / (float) dx;
+      m = (double) (Crosshair.X - line->Point1.X) / (Crosshair.Y - line->Point1.Y);
       m2 = 0;
       if (m > TAN_30_DEGREE)
 	m2 = (m > TAN_60_DEGREE) ? 2 : 1;

src/copy.c
@@ -72,7 +72,7 @@ static void *CopyElement (ElementTypePtr);
 /* ---------------------------------------------------------------------------
  * some local identifiers
  */
-static LocationType DeltaX, DeltaY;	/* movement vector */
+static Coord DeltaX, DeltaY;	/* movement vector */
 static ObjectFunctionType CopyFunctions = {
   CopyLine,
   CopyText,
@@ -119,8 +119,8 @@ CopyPolygonLowLevel (PolygonTypePtr Dest, PolygonTypePtr Src)
  */
 ElementTypePtr
 CopyElementLowLevel (DataTypePtr Data, ElementTypePtr Dest,
-		     ElementTypePtr Src, bool uniqueName, LocationType dx,
-		     LocationType dy)
+		     ElementTypePtr Src, bool uniqueName, Coord dx,
+		     Coord dy)
 {
   int i;
   /* release old memory if necessary */
@@ -322,7 +322,7 @@ CopyElement (ElementTypePtr Element)
  * are handled by the routine.
  */
 bool
-CopyPastebufferToLayout (LocationType X, LocationType Y)
+CopyPastebufferToLayout (Coord X, Coord Y)
 {
   Cardinal i;
   bool changed = false;
@@ -418,7 +418,7 @@ CopyPastebufferToLayout (LocationType X, LocationType Y)
  */
 void *
 CopyObject (int Type, void *Ptr1, void *Ptr2, void *Ptr3,
-	    LocationType DX, LocationType DY)
+	    Coord DX, Coord DY)
 {
   void *ptr;
 

src/copy.h
@@ -43,8 +43,8 @@
 
 PolygonTypePtr CopyPolygonLowLevel (PolygonTypePtr, PolygonTypePtr);
 ElementTypePtr CopyElementLowLevel (DataTypePtr, ElementTypePtr,
-				    ElementTypePtr, bool, LocationType, LocationType);
-bool CopyPastebufferToLayout (LocationType, LocationType);
-void *CopyObject (int, void *, void *, void *, LocationType, LocationType);
+				    ElementTypePtr, bool, Coord, Coord);
+bool CopyPastebufferToLayout (Coord, Coord);
+void *CopyObject (int, void *, void *, void *, Coord, Coord);
 
 #endif

src/insert.h
@@ -38,8 +38,8 @@
 /* ---------------------------------------------------------------------------
  * prototypes
  */
-void *InsertPointIntoObject (int, void *, void *, Cardinal *, LocationType,
-			     LocationType, bool, bool);
+void *InsertPointIntoObject (int, void *, void *, Cardinal *, Coord,
+			     Coord, bool, bool);
 PointTypePtr AdjustInsertPoint (void);
 
 #endif

src/mirror.c
@@ -65,7 +65,7 @@ RCSID ("$Id$");
  */
 void
 MirrorElementCoordinates (DataTypePtr Data, ElementTypePtr Element,
-			  LocationType yoff)
+			  Coord yoff)
 {
   r_delete_element (Data, Element);
   ELEMENTLINE_LOOP (Element);

src/mirror.h
@@ -41,6 +41,6 @@
 #define	MIRROR_TYPES	(TEXT_TYPE | ELEMENTNAME_TYPE)
 
 
-void MirrorElementCoordinates (DataTypePtr, ElementTypePtr, LocationType);
+void MirrorElementCoordinates (DataTypePtr, ElementTypePtr, Coord);
 
 #endif

Nullkraft/PCB-GL

commit 36039faac19406026668a1c3fe21b2b6bcb79583

Fix computation of 45-degree edges when inserting a point into an edge
Fixes a few mistakes introduced in commit b59ab044afe3ac362f8cb4da10ea410280b4323a
which was bisected to and identified as the cuplrit by Gabriel Paubert.

Reported-by: Gabriel Paubert <paubert@iram.es>

src/insert.c
@@ -275,7 +275,7 @@ AdjustInsertPoint (void)
     m1 = 2;			/* 2 signals infinite slope */
   else
     {
-      m = (double) (Crosshair.X - line->Point1.X) / (Crosshair.Y - line->Point1.Y);
+      m = (double) (Crosshair.Y - line->Point1.Y) / (Crosshair.X - line->Point1.X);
       m1 = 0;
       if (m > TAN_30_DEGREE)
 	m1 = (m > TAN_60_DEGREE) ? 2 : 1;
@@ -286,7 +286,7 @@ AdjustInsertPoint (void)
     m2 = 2;			/* 2 signals infinite slope */
   else
     {
-      m = (double) (Crosshair.X - line->Point1.X) / (Crosshair.Y - line->Point1.Y);
+      m = (double) (Crosshair.Y - line->Point2.Y) / (Crosshair.X - line->Point2.X);
       m2 = 0;
       if (m > TAN_30_DEGREE)
 	m2 = (m > TAN_60_DEGREE) ? 2 : 1;