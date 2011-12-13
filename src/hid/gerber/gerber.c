f7519091e347016b3169d417616a750fea68096c
Peter Clifton <pcjc2@cam.ac.uk>
2011-12-13 14:52:36 +0000

    Crasy test
    Note that this throws away code to draw pad and pin names

--- src/hid/gerber/gerber.c
+++ src/hid/gerber/gerber.c
@@ -27,6 +27,7 @@
 #include "error.h"
 #include "draw.h"
 #include "pcb-printf.h"
+#include "draw_funcs.h"
 
 #include "hid.h"
 #include "../hidint.h"
@@ -830,7 +831,7 @@ gerber_set_layer (const char *name, int group, int empty)
     {
       if (outline_layer
 	  && outline_layer != PCB->Data->Layer+idx)
-	DrawLayer (outline_layer, &region);
+	dapi->draw_layer (outline_layer, &region, NULL);
       else if (!outline_layer)
 	{
 	  hidGC gc = gui->make_gc ();
