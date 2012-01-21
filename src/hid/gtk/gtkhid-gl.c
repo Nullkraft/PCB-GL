9b38f1a2af2a063b49e8968a0eccc32a5b8d1aae
Peter Clifton <peter@clifton-electronics.co.uk>
2012-01-21 19:01:13 +0000

    Other drawing API changes

--- src/hid/gtk/gtkhid-gl.c
+++ src/hid/gtk/gtkhid-gl.c
@@ -32,6 +32,7 @@
 #include <dmalloc.h>
 #endif
 
+extern GraphicsAPI ghid_gapi;
 extern HID ghid_hid;
 
 static hidGC current_gc = NULL;
@@ -563,7 +564,7 @@ ghid_fill_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box)
 void
 ghid_thindraw_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box)
 {
-  common_thindraw_pcb_polygon (gc, poly, clip_box);
+  //common_thindraw_pcb_polygon (gc, poly, clip_box);
   ghid_set_alpha_mult (gc, 0.25);
   ghid_fill_pcb_polygon (gc, poly, clip_box);
   ghid_set_alpha_mult (gc, 1.0);
@@ -758,8 +759,8 @@ ghid_init_renderer (int *argc, char ***argv, GHidPort *port)
 
   /* Setup HID function pointers specific to the GL renderer*/
   ghid_hid.end_layer = ghid_end_layer;
-  ghid_hid.fill_pcb_polygon = ghid_fill_pcb_polygon;
-  ghid_hid.thindraw_pcb_polygon = ghid_thindraw_pcb_polygon;
+//  ghid_hid.fill_pcb_polygon = ghid_fill_pcb_polygon;
+//  ghid_hid.thindraw_pcb_polygon = ghid_thindraw_pcb_polygon;
 }
 
 void
@@ -840,6 +841,12 @@ ghid_drawing_area_expose_cb (GtkWidget *widget,
   render_priv *priv = port->render_priv;
   GtkAllocation allocation;
   BoxType region;
+  DrawAPI *dapi;
+
+  dapi = draw_api_new ();
+  dapi->gapi = &ghid_gapi;
+  dapi->gc = dapi->gapi->make_gc ();
+  common_draw_helpers_init (dapi);
 
   gtk_widget_get_allocation (widget, &allocation);
 
@@ -850,8 +857,8 @@ ghid_drawing_area_expose_cb (GtkWidget *widget,
   /* If we don't have any stencil bits available,
      we can't use the hidgl polygon drawing routine */
   /* TODO: We could use the GLU tessellator though */
-  if (hidgl_stencil_bits() == 0)
-    ghid_hid.fill_pcb_polygon = common_fill_pcb_polygon;
+//  if (hidgl_stencil_bits() == 0)
+//    ghid_hid.fill_pcb_polygon = common_fill_pcb_polygon;
 
   glEnable (GL_BLEND);
   glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -918,7 +925,9 @@ ghid_drawing_area_expose_cb (GtkWidget *widget,
 
   hidgl_init_triangle_array (&buffer);
   ghid_invalidate_current_gc ();
-  hid_expose_callback (&ghid_hid, &region, 0);
+  dapi->set_clip_box (dapi, &region);
+  dapi->draw_everything (dapi);
+//  hid_expose_callback (&ghid_hid, &region, 0);
   hidgl_flush_triangles (&buffer);
 
   ghid_draw_grid (&region);
@@ -930,8 +939,8 @@ ghid_drawing_area_expose_cb (GtkWidget *widget,
     dapi = outline_draw_new (gui);
     dapi->set_draw_offset = hidgl_set_draw_offset;
 
-    DrawAttached (dapi);
-    DrawMark (dapi);
+//    DrawAttached (dapi);
+//    DrawMark (dapi);
   }
   hidgl_flush_triangles (&buffer);
 
@@ -1047,7 +1056,7 @@ ghid_pinout_preview_expose (GtkWidget *widget,
                 gport->view.flip_y ? gport->view.y0 - PCB->MaxHeight :
                                     -gport->view.y0, 0);
 
-  hid_expose_callback (&ghid_hid, NULL, &pinout->element);
+//  hid_expose_callback (&ghid_hid, NULL, &pinout->element);
   hidgl_flush_triangles (&buffer);
   glPopMatrix ();
 
@@ -1162,7 +1171,7 @@ ghid_render_pixmap (int cx, int cy, double zoom, int width, int height, int dept
   region.Y1 = MAX (0, MIN (PCB->MaxHeight, region.Y1));
   region.Y2 = MAX (0, MIN (PCB->MaxHeight, region.Y2));
 
-  hid_expose_callback (&ghid_hid, &region, NULL);
+//  hid_expose_callback (&ghid_hid, &region, NULL);
   hidgl_flush_triangles (&buffer);
   glPopMatrix ();
 
