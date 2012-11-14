4d82dcf65ee00b37636948803f16064a0bef5cc5
Peter Clifton <peter@clifton-electronics.co.uk>
2012-11-14 21:59:49 +0000

    Remove HID->set_draw_xor() API
    This is only used for drawing the reference mark and objects attached
    to the cursor. Rather than have the core setup the XOR drawing mode,
    let the GUI set what it wants.
    
    XXX: GTK HID's GDK RENDERER AND LESSTIF HID NOT UPDATED YET

--- src/hid/gcode/gcode.c
+++ src/hid/gcode/gcode.c
@@ -1233,12 +1233,6 @@ gcode_set_line_width (hidGC gc, Coord width)
 }
 
 static void
-gcode_set_draw_xor (hidGC gc, int xor_)
-{
-  ;
-}
-
-static void
 gcode_set_draw_faded (hidGC gc, int faded)
 {
 }
@@ -1601,7 +1595,6 @@ hid_gcode_init ()
   gcode_graphics.set_color      = gcode_set_color;
   gcode_graphics.set_line_cap   = gcode_set_line_cap;
   gcode_graphics.set_line_width = gcode_set_line_width;
-  gcode_graphics.set_draw_xor   = gcode_set_draw_xor;
   gcode_graphics.set_draw_faded = gcode_set_draw_faded;
   gcode_graphics.draw_line      = gcode_draw_line;
   gcode_graphics.draw_arc       = gcode_draw_arc;
