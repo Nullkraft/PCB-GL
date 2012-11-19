a1d6c4fd6e68a3934aafa311581a4e5864c69ca1
Peter Clifton <peter@clifton-electronics.co.uk>
2012-11-19 00:48:26 +0000

    Refresh of remove-thindraw_pcb_polygon-fr

--- src/hid/common/draw_helpers.h
+++ src/hid/common/draw_helpers.h
@@ -1,4 +1,5 @@
-void common_draw_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box);
+void common_fill_pcb_polygon (hidGC gc, PolygonType *poly,
+                              const BoxType *clip_box);
 void common_thindraw_pcb_polygon (hidGC gc, PolygonType *poly,
                                   const BoxType *clip_box);
 void common_fill_pcb_pad (hidGC gc, PadType *pad, bool clear, bool mask);
