32c71d6020658821b160c636c95189c1a2a9ec33
Peter Clifton <peter@clifton-electronics.co.uk>
2012-11-21 21:04:52 +0000

    Refresh of push-special-case-thindraw-vs-

--- src/hid/common/draw_helpers.c
+++ src/hid/common/draw_helpers.c
@@ -241,6 +241,26 @@ should_compute_no_holes (PolygonType *poly, const BoxType *clip_box)
 #undef BOUNDS_INSIDE_CLIP_THRESHOLD
 
 void
+common_gui_draw_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box)
+{
+  if (TEST_FLAG (THINDRAWFLAG, PCB) || TEST_FLAG (THINDRAWPOLYFLAG, PCB))
+    common_thindraw_pcb_polygon (gc, polygon, drawn_area);
+  else
+    common_fill_pcb_polygon (gc, polygon, drawn_area);
+
+  /* If checking planes, thin-draw any pieces which have been clipped away */
+  if (TEST_FLAG (CHECKPLANESFLAG, PCB) && !TEST_FLAG (FULLPOLYFLAG, polygon))
+    {
+      PolygonType poly = *polygon;
+
+      for (poly.Clipped = polygon->Clipped->f;
+           poly.Clipped != polygon->Clipped;
+           poly.Clipped = poly.Clipped->f)
+        common_thindraw_pcb_polygon (gc, &poly, drawn_area);
+    }
+}
+
+void
 common_fill_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box)
 {
   if (!poly->NoHolesValid)
@@ -573,8 +593,7 @@ common_draw_helpers_init (HID_DRAW *graphics)
   graphics->draw_pcb_line        = common_draw_pcb_line;
   graphics->draw_pcb_text        = common_draw_pcb_text;
 
-  graphics->fill_pcb_polygon     = common_fill_pcb_polygon;
-  graphics->thindraw_pcb_polygon = common_thindraw_pcb_polygon;
+  graphics->draw_pcb_polygon     = common_fill_pcb_polygon;
   graphics->fill_pcb_pad         = common_fill_pcb_pad;
   graphics->thindraw_pcb_pad     = common_thindraw_pcb_pad;
   graphics->fill_pcb_pv          = common_fill_pcb_pv;
