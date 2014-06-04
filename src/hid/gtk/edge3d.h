2aaa3fb565132d61a3ef1c86dd0c121db0e6985f
Peter Clifton <peter@clifton-electronics.co.uk>
2014-06-05 00:20:31 +0100

    More play with quad edge data-structures and STEP export

--- src/hid/gtk/edge3d.h
+++ src/hid/gtk/edge3d.h
@@ -1,11 +1,22 @@
 typedef struct
 {
-  bool is_stitch;
+  /* For edge curves */
+  bool is_stitch; /* Allows us to identify the stitch edge along the side of a cylinder */
+
+  /* For circular curves */
   bool is_round;
   float cx;
   float cy;
+  float cz;
+  float nx;
+  float ny;
+  float nz;
   float radius;
+
+  /* STEP crap - to hell with encapsulation */
+  int infinite_line_identifier;
+  int edge_identifier;
 } edge_info;
 
-edge_info *make_edge_info (bool is_stitch, bool is_round, float cx, float cy, float radius);
+edge_info *make_edge_info (bool is_stitch, bool is_round, float cx, float cy, float cz, float nx, float ny, float nz, float radius);
 void destroy_edge_info (edge_info *info);
