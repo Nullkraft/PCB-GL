6a01f2d70ed2f4de9074eed04638ed84b779b5a9
Peter Clifton <peter@clifton-electronics.co.uk>
2014-06-27 02:41:08 +0100

    Refresh of add-soldermask-layers

--- src/const.h
+++ src/const.h
@@ -50,6 +50,8 @@
  */
 #define BOTTOM_SILK_LAYER       0
 #define TOP_SILK_LAYER          1
+#define BOTTOM_SOLDERMASK_LAYER 2
+#define TOP_SOLDERMASK_LAYER    3
 
 /* ---------------------------------------------------------------------------
  * misc constants
