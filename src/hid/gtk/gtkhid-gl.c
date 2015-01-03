3a91122faf0ea299a5300d37e2f06096ff40239b
Peter Clifton <peter@clifton-electronics.co.uk>
2015-01-03 13:47:45 +0000

    Remove debug rendering for polygons

--- src/hid/gtk/gtkhid-gl.c
+++ src/hid/gtk/gtkhid-gl.c
@@ -850,11 +850,9 @@ ghid_thindraw_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *clip_box)
 {
   double old_alpha_mult = gc->alpha_mult;
   common_thindraw_pcb_polygon (gc, poly, clip_box);
-#if 0
   ghid_set_alpha_mult (gc, gc->alpha_mult * 0.25);
   gui->graphics->fill_pcb_polygon (gc, poly, clip_box);
   ghid_set_alpha_mult (gc, old_alpha_mult);
-#endif
 }
 
 void
@@ -1609,8 +1607,7 @@ fill_board_outline (hidGC gc, const BoxType *drawn_area)
     polygon.BoundingBox = *drawn_area;
   polygon.Flags = NoFlags ();
   SET_FLAG (FULLPOLYFLAG, &polygon);
-//  gui->graphics->fill_pcb_polygon (gc, &polygon, drawn_area);
-  gui->graphics->thindraw_pcb_polygon (gc, &polygon, drawn_area);
+  gui->graphics->fill_pcb_polygon (gc, &polygon, drawn_area);
   poly_FreeContours (&polygon.NoHoles);
 }
 
@@ -2662,7 +2659,7 @@ ghid_drawing_area_expose_cb (GtkWidget *widget,
   glDisable (GL_LIGHTING);
 
   draw_crosshair (priv);
-  //object3d_draw_debug ();
+  object3d_draw_debug ();
 
   hidgl_flush_triangles (&buffer);
 
