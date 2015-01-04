a004358057ba75283771885ddd670afea99e3763
Peter Clifton <peter@clifton-electronics.co.uk>
2015-01-04 21:16:35 +0000

    Add support for a two soldermask layers, defining regions of removed mask
    File-format wise, these work similarly to how silk layers are supported now:
    
    Layer (1 to n "[NAME]")            # "ordinary" design layers
    Layer (n+1 "silk")                 # bottom side silk
    Layer (n+2 "silk")                 # top side silk
    Layer (n+3 "bottom soldermask")    # bottom side soldermask
    Layer (n+4 "top soldermask")       # top side soldermask

--- src/draw.c
+++ src/draw.c
@@ -884,6 +884,8 @@ DrawMask (int side, const BoxType *screen)
       hid_draw_use_mask (gui->graphics, HID_MASK_CLEAR);
     }
 
+  DrawLayer (LAYER_PTR (side == TOP_SIDE ? top_soldermask_layer : bottom_soldermask_layer), screen);
+
   r_search (PCB->Data->pin_tree, screen, NULL, clearPin_callback, NULL);
   r_search (PCB->Data->via_tree, screen, NULL, clearPin_callback, NULL);
   r_search (PCB->Data->pad_tree, screen, NULL, clearPad_callback, &side);
