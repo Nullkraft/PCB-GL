2d58087f13a8bf305c3f1011c79d649c0014a5ac
Peter Clifton <peter@clifton-electronics.co.uk>
2015-01-07 01:48:05 +0000

    Play with locked object rendering
    (HACK HACK HACK HACK HACK)
    
    Did I mention this was a hacky pile of rubbish code.. the main point of
    the exercise is to determine whether the usability is better or not.

--- src/action.c
+++ src/action.c
@@ -1146,7 +1146,6 @@ NotifyMode (void)
 	    DrawElement (element);
 	    Draw ();
 	    SetChangedFlag (true);
-	    hid_actionl ("Report", "Object", NULL);
 	  }
 	else if (type != NO_TYPE)
 	  {
@@ -1161,7 +1160,6 @@ NotifyMode (void)
 		Draw ();
 	      }
 	    SetChangedFlag (true);
-	    hid_actionl ("Report", "Object", NULL);
 	  }
 	break;
       }
