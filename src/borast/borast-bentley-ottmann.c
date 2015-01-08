15c40efd70b7e91c9ee06e1dcea06032b8913c60
Peter Clifton <peter@clifton-electronics.co.uk>
2015-01-08 01:17:10 +0000

    BORAST: Fixup to only draw the passed POLYAREA, not walk the linked list of POLYAREA and draw them all!

--- src/borast/borast-bentley-ottmann.c
+++ src/borast/borast-bentley-ottmann.c
@@ -1375,6 +1375,7 @@ poly_area_to_start_events (POLYAREA                *poly,
                                  counter, outer_contour);
         outer_contour = 0;
       }
+      break;
 
     } while ((pa = pa->f) != poly);
 
@@ -1401,6 +1402,7 @@ bo_poly_to_traps (hidGC gc, POLYAREA *poly, borast_traps_t *traps)
     for (contour = pa->contours; contour != NULL; contour = contour->next)
       num_events += contour->Count;
     /* FIXME: Remove horizontal edges? */
+    break;
   } while ((pa = pa->f) != poly);
 
   if (unlikely (0 == num_events))
