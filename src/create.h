571d73def3fc56973fb8776a52170c8cc0ad4544
Peter Clifton <peter@clifton-electronics.co.uk>
2015-01-10 16:02:09 +0000

    Add support for arc segments in polygon object outline definitions
    Test with a polygon like this:
    
      Polygon("clearpoly")
      (
        [5.0000mm 7.0000mm included_angle: 90] [7.0000mm 5.0000mm]
        [28.000mm 5.0000mm included_angle: 90] [30.000mm 7.0000mm]
        [30.000mm 28.000mm included_angle: 90] [28.000mm 30.000mm]
        [7.0000mm 30.000mm included_angle: 90] [5.0000mm 28.000mm]
      )
    
    
    NOTE THAT THE ARC BOUNDS ARE NOT YET INCLUDED IN THE BOUNDING BOX CALCULATION,
    SO THIS WILL BREAK VARIOUS THINGS PRETTY HORRIBLY IF AN ARC PROTRUDES AS AN
    EXTREME FEATURE IN EITHER THE +/-X or +/-Y DIRECTIONS.

--- src/create.h
+++ src/create.h
@@ -51,7 +51,7 @@ ArcType * CreateNewArcOnLayer (LayerType *, Coord, Coord, Coord, Coord, Angle, A
 PolygonType * CreateNewPolygonFromRectangle (LayerType *, Coord, Coord, Coord, Coord, FlagType);
 TextType * CreateNewText (LayerType *, FontType *, Coord, Coord, unsigned, int, char *, FlagType);
 PolygonType * CreateNewPolygon (LayerType *, FlagType);
-PointType * CreateNewPointInPolygon (PolygonType *, Coord, Coord);
+PointType * CreateNewPointInPolygon (PolygonType *, Coord, Coord, Angle included_angle);
 PolygonType * CreateNewHoleInPolygon (PolygonType *polygon);
 void RefdesMapInit (void);
 ElementType * CreateNewElement (DataType *, FontType *, FlagType, char *, char *, char *, Coord, Coord, BYTE, int, FlagType, bool);
