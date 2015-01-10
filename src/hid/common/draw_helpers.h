812912e6e05d35300c90ea96188e4bd8ee41be8c
Peter Clifton <peter@clifton-electronics.co.uk>
2015-01-10 13:45:02 +0000

    Split pin/via shape and hole drawing into different functions
    Mostly we only need to draw one at once, and using the same function for both is
    unnecessarily complicated.
    
    ** Known to break rendering of the thin outline around a pure drill-hole
       when not viewing in THINDRAW or THINDRAWPOLY mode. This is because the old
       monolithic pin/via drawing routine would re-draw that otline on top of the
       masked out drill region.
    
       With this, either we need to have the fill_pcb_pv_hole() routine temporarily
       setup and draw in the foreground colour, OR.. a third (or re-ordered) pass
       needs to be made for highlighting the rim of a pure hole in normal view mode.
    
       This must not be done of course, exporters requiring physical object accuracy.
    
       OpenGL (with its stencil based sub-compositing) will need a different render
       order (if sub-compositing is turned on) than say, the GDK or X11 based HIDs
       which don't have any transparency, and just paint in Z-order of visible feature.

--- src/hid/common/draw_helpers.h
+++ src/hid/common/draw_helpers.h
@@ -4,7 +4,10 @@ void common_thindraw_pcb_polygon (hidGC gc, PolygonType *poly, const BoxType *cl
 void common_gui_draw_pcb_pad (hidGC gc, PadType *pad, bool clear, bool mask);
 void common_fill_pcb_pad (hidGC gc, PadType *pad, bool clear, bool mask);
 void common_thindraw_pcb_pad (hidGC gc, PadType *pad, bool clear, bool mask);
-void common_gui_draw_pcb_pv (hidGC fg_gc, hidGC bg_gc, PinType *pv, bool drawHole, bool mask);
-void common_fill_pcb_pv (hidGC fg_gc, hidGC bg_gc, PinType *pv, bool drawHole, bool mask);
-void common_thindraw_pcb_pv (hidGC fg_gc, hidGC bg_gc, PinType *pv, bool drawHole, bool mask);
+void common_gui_draw_pcb_pv (hidGC gc, PinType *pv, bool mask);
+void common_gui_draw_pcb_pv_hole (hidGC gc, PinType *pv);
+void common_fill_pcb_pv (hidGC gc, PinType *pv, bool mask);
+void common_fill_pcb_pv_hole (hidGC gc, PinType *pv);
+void common_thindraw_pcb_pv (hidGC gc, PinType *pv, bool mask);
+void common_thindraw_pcb_pv_hole (hidGC gc, PinType *pv);
 void common_draw_helpers_init (HID_DRAW *graphics);
