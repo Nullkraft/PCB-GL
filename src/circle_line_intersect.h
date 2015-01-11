e251b29c75c85c41a621153e2b744dbb3a19aab6
Peter Clifton <peter@clifton-electronics.co.uk>
2015-01-11 23:41:01 +0000

    Attempt at supporting arc-arc intersections in polygons

---
+++ src/circle_line_intersect.h
@@ -0,0 +1,3 @@
+int circle_line_intersect (double cx, double cy, double r,
+                           double p1x, double p1y, double p2x, double p2y,
+                           double *mu1, double *mu2);
