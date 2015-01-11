bcc80f9b410436acad12217d71c4774cb313bea4
Peter Clifton <peter@clifton-electronics.co.uk>
2015-01-11 15:40:32 +0000

    Avoid hard-coding the number of extra layers in use (for 2x silkscreen)
    We will shorty add support for soldermask layers, so this will need changing
    
    
    NOTE....... IF THE parse_y.y patch in the stg series before this gets
    dropped, there is one more case where + 2 needs to be replaced with EXTRA_LAYERS

--- src/crosshair.c
+++ src/crosshair.c
@@ -270,7 +270,7 @@ XORDrawBuffer (hidGC gc, BufferType *Buffer)
   y = Crosshair.Y - Buffer->Y;
 
   /* draw all visible layers */
-  for (i = 0; i < max_copper_layer + 2; i++)
+  for (i = 0; i < max_copper_layer + EXTRA_LAYERS; i++)
     if (PCB->Data->Layer[i].On)
       {
 	LayerType *layer = &Buffer->Data->Layer[i];
