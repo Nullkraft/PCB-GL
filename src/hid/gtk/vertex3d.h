bfc752ee22a57b027e32bf89da22f700d5b85c2e
Peter Clifton <peter@clifton-electronics.co.uk>
2015-03-21 11:51:09 +0000

    More play with quad edge data-structures and STEP export

--- src/hid/gtk/vertex3d.h
+++ src/hid/gtk/vertex3d.h
@@ -4,6 +4,9 @@ typedef struct
   float y;
   float z;
   int id;
+
+  /* STEP crap - to hell with encapsulation */
+  int vertex_identifier;
 } vertex3d;
 
 vertex3d *make_vertex3d (float x, float y, float z);
