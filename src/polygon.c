1ba9eec059e37e500c51a7aaa016dfec606e5008
Peter Clifton <peter@clifton-electronics.co.uk>
2015-04-12 17:08:54 +0100

    Speedup for models whilst debugging

--- src/polygon.c
+++ src/polygon.c
@@ -2177,6 +2177,8 @@ pv_outline_callback (const BoxType * b, void *cl)
     }
   else
     {
+      /* NOT FROM ELEMENT, DROP HOLE */
+      return 1;
       feature_name = STRDUP(pv->Name);
     }
 
