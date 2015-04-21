8d686351f7921a1f24c0bf7b39f1208861b4b4f3
Peter Clifton <pcjc2@cam.ac.uk>
2015-04-21 02:08:26 +0100

    Convert object memory allocations to use g_slice()

--- src/autoroute.c
+++ src/autoroute.c
@@ -1015,7 +1015,6 @@ CreateRouteData ()
     {
       layergroupboxes[i].Ptr = NULL;
       layergroupboxes[i].PtrN = 0;
-      layergroupboxes[i].PtrMax = 0;
       GROUP_LOOP (PCB->Data, i);
       {
 	if (layer->LineN || layer->ArcN)
