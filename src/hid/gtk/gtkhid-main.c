470cef3a0ddc856398ce82d2c26fc229d20602fa
Peter Clifton <peter@clifton-electronics.co.uk>
2016-01-06 17:22:04 +0000

    TEMP: Fixup fallout from nogui change
    *** Might be nicer to just implement a no-op end_layer function for each HID_DRAW?

--- src/hid/gtk/gtkhid-main.c
+++ src/hid/gtk/gtkhid-main.c
@@ -2182,6 +2182,7 @@ hid_gtk_init ()
 
   ghid_hid.graphics                 = &ghid_graphics;
 
+  common_nogui_graphics_class_init (&ghid_graphics_class);
   common_draw_helpers_class_init (&ghid_graphics_class);
 
   ghid_graphics_class.set_layer      = ghid_set_layer;
@@ -2205,6 +2206,7 @@ hid_gtk_init ()
 
   ghid_graphics.klass = &ghid_graphics_class;
   ghid_graphics.poly_after = true;
+  common_nogui_graphics_init (&ghid_graphics);
   common_draw_helpers_init (&ghid_graphics);
 
   hid_register_hid (&ghid_hid);
