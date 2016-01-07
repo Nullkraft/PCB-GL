b72eb10612c98c75fdebab748a4cfee7eb4d57bc
Peter Clifton <peter@clifton-electronics.co.uk>
2016-01-07 01:46:26 +0000

    Add clip_box member to the HID_DRAW * instance structure

--- src/hid/common/draw_helpers.h
+++ src/hid/common/draw_helpers.h
@@ -12,3 +12,4 @@ void common_thindraw_pcb_pv (hidGC gc, PinType *pv, bool mask);
 void common_thindraw_pcb_pv_hole (hidGC gc, PinType *pv);
 void common_draw_helpers_class_init (HID_DRAW_CLASS *klass);
 void common_draw_helpers_init (HID_DRAW *graphics);
+void common_set_clip_box (HID_DRAW *hid_draw, BoxType *clip_box);
