adc7a2f09d5df0c5957819cfdf27e6d1548ef40f
Peter Clifton <peter@clifton-electronics.co.uk>
2016-01-07 01:46:27 +0000

    Hidcairo

---
+++ src/hid/common/hidcairo.h
@@ -0,0 +1,6 @@
+void hidcairo_class_init (HID_DRAW_CLASS *klass);
+void hidcairo_init (HID_DRAW *hid_draw);
+void hidcairo_start_render (HID_DRAW *hid_draw, cairo_t *cr);
+void hidcairo_finish_render (HID_DRAW *hid_draw);
+
+//extern HID_DRAW cairo_graphics;
