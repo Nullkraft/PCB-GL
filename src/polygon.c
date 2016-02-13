7bf818bcae887bb96508e3fad8e07b241362a345
Peter Clifton <peter@clifton-electronics.co.uk>
2016-02-13 18:26:13 +0000

    Fix board outline generation to not crash when we find nothing
    This was hit due to another bug, but since NULL is a valid polygon, we
    should handle this case!

--- src/polygon.c
+++ src/polygon.c
@@ -2458,8 +2458,8 @@ POLYAREA *board_outline_poly (bool include_holes)
   /* Now we just need to work out which pieces of polygon are inside
      and outside the board! */
 
-  /* If there is only one piece, return that */
-  if (clipped->f == clipped)
+  /* If there is no result, or only one piece, return that */
+  if (clipped == NULL || clipped->f == clipped)
     return clipped;
 
   /* WARNING: This next check is O(n^2), where n is the number of clipped
