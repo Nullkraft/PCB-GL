71a7a5a92291a9e9249ce13c3d8d81d47e12e131
Peter Clifton <peter@clifton-electronics.co.uk>
2016-02-13 18:57:49 +0000

    More play with quad edge data-structures and STEP export

--- src/hid/gtk/face3d.h
+++ src/hid/gtk/face3d.h
@@ -1,8 +1,25 @@
 typedef struct {
   float nx, ny, nz; /* Face normal?*/
   GList *contours;
+
+  /* For cylindrical surfaces */
+  bool is_cylindrical;
+  float cx, cy, cz; /* A point on the axis */
+  float ax, ay, az; /* Direction of the axis */
+  float radius;
+
+  appearance *appear;
+
+  /* STEP crap - to hell with encapsulation */
+  int plane_identifier;
+  bool plane_orientation_reversed;
+  int face_identifier;
+  int face_bound_identifier;
 } face3d;
 
 face3d *make_face3d (void);
 void destroy_face3d (face3d *face);
-void face3d_add_contour (face3d *face, edge_ref contour);
+void face3d_add_contour (face3d *face, contour3d *contour);
+void face3d_set_appearance (face3d *face, appearance *appear);
+void face3d_set_normal (face3d *face, float nx, float ny, float nz);
+void face3d_set_cylindrical (face3d *face, float cx, float cy, float cz, float ax, float ay, float az, float radius);
