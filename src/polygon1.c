fd1220d51e8d91ebd7737bd296b2879a33b0b770
Peter Clifton <peter@clifton-electronics.co.uk>
2016-02-18 14:11:32 +0000

    Add name tags to contours and feature naming for pins and vias in STEP export

--- src/polygon1.c
+++ src/polygon1.c
@@ -2601,6 +2601,7 @@ poly_IniContour (PLINE * c)
   c->cx = 0;
   c->cy = 0;
   c->radius = 0;
+  c->name = NULL;
 }
 
 PLINE *
@@ -2635,6 +2636,8 @@ poly_ClrContour (PLINE * c)
   free (c->tristrip_vertices);
   c->tristrip_vertices = NULL;
   c->tristrip_num_vertices = 0;
+  free (c->name);
+  c->name = NULL;
   poly_IniContour (c);
 }
 
@@ -2667,6 +2670,7 @@ poly_DelContour (PLINE ** c)
       r_destroy_tree (&r);
     }
   free ((*c)->tristrip_vertices);
+  free ((*c)->name);
   free (*c), *c = NULL;
 }
 
