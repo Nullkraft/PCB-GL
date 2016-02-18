1e2658101c16416852db4d625f1c8dbd3cc9944c
Peter Clifton <peter@clifton-electronics.co.uk>
2016-02-18 14:11:33 +0000

    Alternative, more thorough STEP export code

---
+++ src/hid/common/edge3d.h
@@ -0,0 +1,24 @@
+typedef struct
+{
+  /* For edge curves */
+  bool is_stitch; /* Allows us to identify the stitch edge along the side of a cylinder */
+
+  /* For circular curves */
+  bool is_round;
+  double cx;
+  double cy;
+  double cz;
+  double nx;
+  double ny;
+  double nz;
+  double radius;
+
+  /* STEP crap - to hell with encapsulation */
+  step_id infinite_line_identifier;
+  step_id edge_identifier;
+} edge_info;
+
+edge_info *make_edge_info (void);
+void edge_info_set_round (edge_info *info, double cx, double cy, double cz, double nx, double ny, double nz, double radius);
+void edge_info_set_stitch (edge_info *info);
+void destroy_edge_info (edge_info *info);
