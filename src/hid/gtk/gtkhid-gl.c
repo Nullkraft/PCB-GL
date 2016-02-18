2a0b5276c78912d423638cd056e7cab36b8a7004
Peter Clifton <peter@clifton-electronics.co.uk>
2016-02-18 17:48:47 +0000

    XXX: SEE BELOW.. Alternative, more thorough STEP export code
    This has mostly been copied and refactored from the 3D code in the branch
    local_customisation_pcb+gl. They MAY have diverged, and a common history
    should be recreated if possible.

--- src/hid/gtk/gtkhid-gl.c
+++ src/hid/gtk/gtkhid-gl.c
@@ -15,12 +15,13 @@
 #include "gui-pinout-preview.h"
 #include "pcb-printf.h"
 
-#include "hid/step/step_writer.h"
+#include "hid/common/step_id.h"
 #include "hid/common/quad.h"
 #include "hid/common/vertex3d.h"
 #include "hid/common/contour3d.h"
 #include "hid/common/appearance.h"
 #include "hid/common/face3d.h"
+#include "hid/common/edge3d.h"
 #include "hid/common/object3d.h"
 
 #ifndef WIN32
