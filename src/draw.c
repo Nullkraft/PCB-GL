0192602884881affef05239a1a40ebc028613efa
Peter Clifton <pcjc2@cam.ac.uk>
2016-02-18 20:35:24 +0000

    Comment out check for GUI HID when considering hidenames
    Yes - I want to hide them for a print!

--- src/draw.c
+++ src/draw.c
@@ -184,7 +184,7 @@ DrawStrippedText (hidGC gc, ElementType *Element, int min_width)
 static void
 draw_element_name (ElementType *element)
 {
-  if ((TEST_FLAG (HIDENAMESFLAG, PCB) && hid_draw_is_gui (hid_draw)) ||
+  if ((TEST_FLAG (HIDENAMESFLAG, PCB) /*&& hid_draw_is_gui (hid_draw)*/) ||
       TEST_FLAG (HIDENAMEFLAG, element))
     return;
   ghid_set_lock_effects (Output.fgGC, (AnyObjectType *)element);
