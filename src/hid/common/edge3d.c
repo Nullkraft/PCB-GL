0a67d54af873ea42ccebc7ff8fb34d1b17dc735c
Peter Clifton <peter@clifton-electronics.co.uk>
2016-02-18 14:11:33 +0000

    Alternative, more thorough STEP export code

---
+++ src/hid/common/edge3d.c
@@ -0,0 +1,41 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <glib.h>
+
+#include "step_id.h"
+#include "edge3d.h"
+
+edge_info *
+make_edge_info (void)
+{
+  edge_info *info;
+
+  info = g_new0 (edge_info, 1);
+
+  return info;
+}
+
+void
+edge_info_set_round (edge_info *info, double cx, double cy, double cz, double nx, double ny, double nz, double radius)
+{
+  info->is_round = true;
+  info->cx = cx;
+  info->cy = cy;
+  info->cz = cz;
+  info->nx = nx;
+  info->ny = ny;
+  info->nz = nz;
+  info->radius = radius;
+}
+
+void edge_info_set_stitch (edge_info *info)
+{
+  info->is_stitch = true;
+}
+
+void
+destroy_edge_info (edge_info *info)
+{
+  g_free (info);
+}
