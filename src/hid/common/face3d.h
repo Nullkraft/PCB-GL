3cd0bcf885c51e016dcf43d1371999f4ba801519
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-05 16:25:59 +0000

    Continue tinkering with 3D games

--- src/hid/common/face3d.h
+++ src/hid/common/face3d.h
@@ -18,3 +18,4 @@ void face3d_add_contour (face3d *face, contour3d *contour);
 void face3d_set_appearance (face3d *face, appearance *appear);
 void face3d_set_normal (face3d *face, double nx, double ny, double nz);
 void face3d_set_cylindrical (face3d *face, double cx, double cy, double cz, double ax, double ay, double az, double radius);
+void face3d_set_surface_orientation_reversed (face3d *face);
