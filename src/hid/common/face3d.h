bef99d88919b85ce2d0eedcd8ed1d2bb48f444a5
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-05 16:25:59 +0000

    Continue tinkering with 3D games

--- src/hid/common/face3d.h
+++ src/hid/common/face3d.h
@@ -11,8 +11,8 @@ typedef struct {
   appearance *appear;
 
   /* XXX: STEP specific - breaks encapsulation */
-  int plane_identifier;
-  bool plane_orientation_reversed;
+  int surface_identifier;
+  bool surface_orientation_reversed;
   int face_identifier;
   int face_bound_identifier;
 } face3d;
@@ -23,3 +23,4 @@ void face3d_add_contour (face3d *face, contour3d *contour);
 void face3d_set_appearance (face3d *face, appearance *appear);
 void face3d_set_normal (face3d *face, double nx, double ny, double nz);
 void face3d_set_cylindrical (face3d *face, double cx, double cy, double cz, double ax, double ay, double az, double radius);
+void face3d_set_surface_orientation_reversed (face3d *face);
