cc7e0b0dae61c74085ed4c9ea98d65717976d6c7
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-05 16:25:59 +0000

    Play with quad-edge data-structure - using PCB as a framework for GL!

---
+++ src/hid/common/vertex3d.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+static int global_vertex3d_count;
+
+typedef struct
+{
+  double x;
+  double y;
+  double z;
+  int id;
+} vertex3d;
+
+vertex3d *
+make_vertex3d (double x, double y, double z)
+{
+  vertex3d *v;
+
+  v = malloc (sizeof(vertex3d));
+  v->x = x;
+  v->y = y;
+  v->z = z;
+  v->id = global_vertex3d_count++;
+
+  return v;
+}
+
+void
+destroy_vertex3d (vertex3d *v)
+{
+  free (v);
+}
