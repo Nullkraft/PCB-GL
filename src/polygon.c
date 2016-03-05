3dad7dcf36965c74ce022db372ab70f67e1ad17b
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-05 16:20:42 +0000

    Fixup board_outline_poly() to subtract vias / pins even if no outline was found

--- src/polygon.c
+++ src/polygon.c
@@ -2092,10 +2092,8 @@ POLYAREA *board_outline_poly (bool include_holes)
         }
     }
 
-  if (!found_outline) {
+  if (!found_outline)
     printf ("Didn't find outline\n");
-    return whole_world;
-  }
 
   /* Do stuff to turn the outline layer into a polygon */
 
@@ -2127,8 +2125,11 @@ POLYAREA *board_outline_poly (bool include_holes)
   region.X2 = PCB->MaxWidth;
   region.Y2 = PCB->MaxHeight;
 
-  r_search (Layer->line_tree, &region, NULL, line_outline_callback, &info);
-  r_search (Layer->arc_tree,  &region, NULL, arc_outline_callback, &info);
+  if (found_outline)
+    {
+      r_search (Layer->line_tree, &region, NULL, line_outline_callback, &info);
+      r_search (Layer->arc_tree,  &region, NULL, arc_outline_callback, &info);
+    }
 
   if (include_holes)
     {
