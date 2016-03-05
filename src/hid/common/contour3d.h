05fcd3bddb8aa0c73ad762b88775161f5e07c907
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-05 16:25:59 +0000

    More play with quad edge data-structures and STEP export

---
+++ src/hid/common/contour3d.h
@@ -0,0 +1,9 @@
+typedef struct {
+  edge_ref first_edge;
+
+  /* XXX: STEP specific - breaks encapsulation */
+  int face_bound_identifier;
+} contour3d;
+
+contour3d *make_contour3d (edge_ref first_edge);
+void destroy_contour3d (contour3d *contour);
