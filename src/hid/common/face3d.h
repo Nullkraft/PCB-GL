34a60e064997481f2a1ec2333b73b500dff16e08
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-06 11:28:34 +0000

    Revert "Ripple out STEP emission 3"
    This reverts commit 421eac19ec17803564746687136078731cea1e58.

--- src/hid/common/face3d.h
+++ src/hid/common/face3d.h
@@ -9,6 +9,12 @@ typedef struct {
   double radius;
 
   appearance *appear;
+
+  /* XXX: STEP specific - breaks encapsulation */
+  int surface_identifier;
+  bool surface_orientation_reversed;
+  int face_identifier;
+  int face_bound_identifier;
 } face3d;
 
 face3d *make_face3d (void);
