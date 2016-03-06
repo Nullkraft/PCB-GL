81c528dbc1becf608233817e2ff21a3cd0a7055c
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-06 16:18:11 +0000

    Refactor to use step_id, not int

--- src/hid/common/vertex3d.h
+++ src/hid/common/vertex3d.h
@@ -6,7 +6,7 @@ typedef struct
   int id;
 
   /* XXX: STEP specific - breaks encapsulation */
-  int vertex_identifier;
+  step_id vertex_identifier;
 } vertex3d;
 
 vertex3d *make_vertex3d (double x, double y, double z);
