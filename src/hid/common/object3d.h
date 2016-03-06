71aa6d819e7269669f53750286e818728b024443
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-06 11:08:29 +0000

    STEP stuff from play-with-quad-edge-data

--- src/hid/common/object3d.h
+++ src/hid/common/object3d.h
@@ -9,3 +9,5 @@ object3d *make_object3d (void);
 void destroy_object3d (object3d *object);
 object3d *object3d_create_test_cube (void);
 object3d *object3d_from_board_outline (void);
+void object3d_export_to_step (object3d *object, const char *filename);
+void object3d_test_board_outline (void);
