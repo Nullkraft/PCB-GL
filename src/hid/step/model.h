c138317c3338ed7f2b4a3dee52f25e883df8c367
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-06 14:59:08 +0000

    Toy rendering attempt

---
+++ src/hid/step/model.h
@@ -0,0 +1,23 @@
+
+struct step_model_instance {
+  double x;
+  double y;
+  double rotation;
+};
+
+
+struct step_model {
+  const char *filename;
+  GList *instances;
+  double ox;
+  double oy;
+  double oz;
+  double ax;
+  double ay;
+  double az;
+  double rx;
+  double ry;
+  double rz;
+};
+
+struct step_model *step_model_to_shape_master (const char *filename, object3d **out_object);
