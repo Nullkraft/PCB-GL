ba12b7b7507cf1f9dec2e392922054f287d752c6
Peter Clifton <peter@clifton-electronics.co.uk>
2016-03-12 19:14:01 +0000

    Refresh of convert-layer-and-group

--- src/misc.c
+++ src/misc.c
@@ -614,7 +614,7 @@ SetTextBoundingBox (FontType *FontPtr, TextType *Text)
 {
   SymbolType *symbol = FontPtr->Symbol;
   unsigned char *s = (unsigned char *) Text->TextString;
-  unsigned int i;
+  int i;
   int space;
 
   Coord minx, miny, maxx, maxy, tx;
