0bdb8b7cc88c3eeca7c33475e6327fc341369e2a
Peter Clifton <peter@clifton-electronics.co.uk>
2016-12-04 19:20:04 +0000

    Add cache for board-outline
    This adds a cache of the board outline, to improve rendering speed
    when plotting the board with an outline.
    
    The cache is invalidated when the "outline" or "route" layer is
    updated. We catch this by bodging the ClearFromPolygon() routine,
    which is called for almost all layer geometry changes which might
    affect us.
    
    XXX: We really need to add a nicer place to be notified of board
    layer changes!

--- src/polygon.c
+++ src/polygon.c
@@ -1633,6 +1633,16 @@ RestoreToPolygon (DataType * Data, int type, void *ptr1, void *ptr2)
   if (!Data->polyClip)
     return;
 
+  /* XXX: HACK, this is a kludgy place to put a check for recomputing our outline */
+  if (type == LINE_TYPE || type == ARC_TYPE)
+    {
+      LayerType *layer = (LayerType *) ptr1;
+
+      if (strcmp (layer->Name, "outline") == 0 ||
+          strcmp (layer->Name, "route") == 0)
+        Data->outline_valid = false;
+    }
+
   if (type == POLYGON_TYPE)
     InitClip (PCB->Data, (LayerType *) ptr1, (PolygonType *) ptr2);
   else
@@ -1644,6 +1654,16 @@ ClearFromPolygon (DataType * Data, int type, void *ptr1, void *ptr2)
 {
   if (!Data->polyClip)
     return;
+
+  /* XXX: HACK, this is a kludgy place to put a check for recomputing our outline */
+  if (type == LINE_TYPE || type == ARC_TYPE)
+    {
+      LayerType *layer = (LayerType *) ptr1;
+
+      if (strcmp (layer->Name, "outline") == 0 ||
+          strcmp (layer->Name, "route") == 0)
+        Data->outline_valid = false;
+    }
 
   if (type == POLYGON_TYPE)
     InitClip (PCB->Data, (LayerType *) ptr1, (PolygonType *) ptr2);
