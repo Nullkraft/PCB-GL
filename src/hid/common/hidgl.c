cc02241e47ac9794dc8e5151aa02187cc8ecc6a0
Peter Clifton <peter@clifton-electronics.co.uk>
2016-12-18 19:36:45 +0000

    More restructuring on 3D model rendering

--- src/hid/common/hidgl.c
+++ src/hid/common/hidgl.c
@@ -257,7 +257,7 @@ hidgl_flush_triangles (hidgl_instance *hidgl)
 }
 
 void
-hidgl_ensure_vertex_space (hidGC gc, int count)
+hidgl_ensure_vertex_space (hidGC gc, unsigned int count)
 {
   hidglGC hidgl_gc = (hidglGC)gc;
   hidgl_instance *hidgl = hidgl_gc->hidgl;
@@ -277,7 +277,7 @@ hidgl_ensure_vertex_space (hidGC gc, int count)
 }
 
 void
-hidgl_ensure_triangle_space (hidGC gc, int count)
+hidgl_ensure_triangle_space (hidGC gc, unsigned int count)
 {
   CHECK_IS_IN_CONTEXT ();
 
